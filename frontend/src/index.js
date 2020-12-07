5497ca53ec52d1630b84e6f47b56c2d0d1943271
Aaryan Sharma <60098288+Aaryan8751@users.noreply.github.com>
2020-12-08 01:45:11 +0530

    Header, Footer and navbar done
    Header, Footer and navbar done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap-social/bootstrap-social.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(

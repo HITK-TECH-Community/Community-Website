7b5d2951fe025aee1a0c906d59119fa48f32c0a6
Aaryan Sharma <60098288+Aaryan8751@users.noreply.github.com>
2020-12-09 21:16:40 +0530

    Login_Signup_Frontend

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,15 +1,18 @@
 import './app.css';
 import Footer from  './components/header_and_footer/Aaryan/footer';
-
-import Header from './components/header_and_footer/Aaryan/header'
+import Header from './components/header_and_footer/Aaryan/header';
+import LoginForm from './components/login_and_signup/Aaryan/login';
+import SignupForm from './components/login_and_signup/Aaryan/signup'
 import {BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-          <Header />
-          <Footer />
+         <Header />
+          <LoginForm />
+          <SignupForm />
+        <Footer />
       </BrowserRouter>
      
     </div>

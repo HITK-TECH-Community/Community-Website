9518bf68b05fa6b3b05276d94c58e44a117b0131
Aaryan Sharma <60098288+Aaryan8751@users.noreply.github.com>
2020-12-10 12:28:01 +0530

    Revert "Login_Signup_Frontend"
    This reverts commit 7b5d2951fe025aee1a0c906d59119fa48f32c0a6.

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,18 +1,15 @@
 import './app.css';
 import Footer from  './components/header_and_footer/Aaryan/footer';
-import Header from './components/header_and_footer/Aaryan/header';
-import LoginForm from './components/login_and_signup/Aaryan/login';
-import SignupForm from './components/login_and_signup/Aaryan/signup'
+
+import Header from './components/header_and_footer/Aaryan/header'
 import {BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-         <Header />
-          <LoginForm />
-          <SignupForm />
-        <Footer />
+          <Header />
+          <Footer />
       </BrowserRouter>
      
     </div>

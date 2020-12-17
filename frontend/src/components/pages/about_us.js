94d5a45af01fd7786b0f06d43aee607bcaa27739
Reet Priye <reetpriye@gmail.com>
2020-12-17 08:12:32 +0530

    Update homepage

---
+++ frontend/src/components/pages/about_us.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const AboutUs = () => <h1 className='about-us'>ABOUT US</h1>;
+
+export default AboutUs;
---
+++ frontend/src/components/pages/broadcasts.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Broadcasts = () => <h1 className='broadcasts'>BROADCASTS</h1>;
+
+export default Broadcasts;

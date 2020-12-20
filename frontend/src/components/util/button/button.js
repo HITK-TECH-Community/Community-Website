8a94fc21038008c2834f81e4456c59b5caa3e125
Abhishek Mogaveera <37222497+Abhishek971999@users.noreply.github.com>
2020-12-21 01:03:41 +0530

    Contact Page updated

--- frontend/src/components/util/button/button.js
+++ frontend/src/components/util/button/button.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
-import './button.css';
+import "./button.css";
 
 const Button = (props) => {
   const { text, path } = props;
 
   return (
     <Link to={path}>
-      <button className='btn'>
+      <button className="btn">
         {text}
         <span>?</span>
       </button>
@@ -23,3 +23,19 @@ Button.propTypes = {
 };
 
 export default Button;
+
+export const Button1 = (props) => {
+  return (
+    <button type={props?.type || "button"} className="main-btn">
+      {props?.label}
+    </button>
+  );
+};
+
+export const Button2 = (props) => {
+  return (
+    <button type={props?.type || "button"} className="main-btn main-btn-2">
+      {props?.label}
+    </button>
+  );
+};

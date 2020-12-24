2ab4a32bacd05ed2334ddf7bd33ee38c79d4c034
Abhishek Mogaveera <37222497+Abhishek971999@users.noreply.github.com>
2020-12-24 21:53:41 +0530

    Carousel_updated
    * Updated carousel component
    
    * Navbar admin button bug fixed
    
    * Carousel bug fixed

--- frontend/src/components/home/carousel/carousel.js
+++ frontend/src/components/home/carousel/carousel.js
@@ -1,30 +1,41 @@
-import React,{Component} from 'react';  
-import OwlCarousel from 'react-owl-carousel';  
-import 'owl.carousel/dist/assets/owl.carousel.css';  
-import 'owl.carousel/dist/assets/owl.theme.default.css';  
-import './carousel.css';  
-export class Owldemo1 extends Component {  
-        render()  
-        {  
-          return (  
-            <div className="carousel-div"> 
-            <h1>Curious to know More about Us?</h1>    
-        <OwlCarousel items={3}  
-          className="owl-theme"  
-          loop  
-          nav  
-          margin={10} 
-          autoplay ={true}
-          >  
-           <div ><img  className="img" alt="" src= "./images/help.png"/></div>  
-           <div><img  className="img" alt="" src= "./images/aim.png"/></div>  
-           <div><img  className="img" alt="" src= "./images/outcome.png"/></div>  
-           <div><img  className="img" alt="" src= "./images/about.png"/></div>  
-      </OwlCarousel>   
-      </div>  
-          )  
-        }  
-      }  
+import React, { Component } from "react";
+import OwlCarousel from "react-owl-carousel";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
+import "./carousel.css";
+import "../motive/motive.css";
+export class Owldemo1 extends Component {
+  render() {
+    return (
+      <div className="carousel-div">
+        <div className="motive">
+          <h1 style={{ color: "#121f3a" }}>Curious to know More about Us?</h1>
+          <div className="dash"></div>
+        </div>
+        <OwlCarousel
+          items={3}
+          className="owl-theme"
+          loop
+          nav
+          margin={10}
+          autoplay={true}
+        >
+          <div>
+            <img className="owl-image" alt="" src="./images/help.png" />
+          </div>
+          <div>
+            <img className="owl-image" alt="" src="./images/aim.png" />
+          </div>
+          <div>
+            <img className="owl-image" alt="" src="./images/outcome.png" />
+          </div>
+          <div>
+            <img className="owl-image" alt="" src="./images/about.png" />
+          </div>
+        </OwlCarousel>
+      </div>
+    );
+  }
+}
 
-
-export default Owldemo1 
+export default Owldemo1;

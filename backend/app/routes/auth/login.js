c976d4e2be17c1433d193d50c7cc836dc5005a88
Rohit Agarwal <rohitagrl1302@gmail.com>
2020-12-26 01:29:24 +0530

    fix(backend login): fixes login route

--- backend/app/routes/auth/login.js
+++ backend/app/routes/auth/login.js
@@ -1,6 +1,5 @@
 const argon2 = require('argon2');
 const Admin = require('../../models/Admin');
-const { generateJWT } = require('../../../helpers/middlewares/auth');
 const { ErrorHandler } = require('../../../helpers/error');
 const constants = require('../../../constants');
 
@@ -15,21 +14,22 @@ module.exports = async (req, res, next) => {
       errStack: 'Auth Login',
     });
     next(error);
-  } else {
-    const correctPassword = await argon2.verify(
-      userRecord.passwordHash,
-      password
-    );
-    if (!correctPassword) {
-      const error = new ErrorHandler(constants.ERRORS.INPUT, {
-        statusCode: 400,
-        message: 'Invalid login credentials',
-        user: email,
-        errStack: 'Auth Login',
-      });
-      next(error);
-    }
+    return false;
   }
-  req.body.data = generateJWT(req.body);
-  res.status(200).send(req.body);
+  const isPasswordCorrect = await argon2.verify(
+    userRecord.passwordHash,
+    password
+  );
+  if (!isPasswordCorrect) {
+    const error = new ErrorHandler(constants.ERRORS.INPUT, {
+      statusCode: 400,
+      message: 'Invalid login credentials',
+      user: email,
+      errStack: 'Auth Login',
+    });
+    next(error);
+    return false;
+  }
+
+  return res.status(204).send();
 };

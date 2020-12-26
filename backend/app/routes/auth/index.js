44be83d2105ec021b95954bccdc5387d970deede
Rohit Agarwal <rohitagrl1302@gmail.com>
2020-12-26 22:09:35 +0530

    feat(backend): Adds request validation middleware
    Implements API request validation middleware using Joi.

--- backend/app/routes/auth/index.js
+++ backend/app/routes/auth/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router({ mergeParams: true });
 const { authMiddleware } = require('../../../helpers/middlewares/auth');
 const login = require('./login');
+const loginSchema = require('./@validationSchema');
+const validation = require('../../../helpers/middlewares/validation');
 
-router.use(authMiddleware);
-router.post('/login', login);
+router.post('/login', authMiddleware, validation(loginSchema), login);
 
 module.exports = router;

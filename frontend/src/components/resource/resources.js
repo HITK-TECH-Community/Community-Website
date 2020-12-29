e63fa21b3784be94fedfd0182464634a4f4c84ce
Vedika Agarwal <67699840+vedikaag99@users.noreply.github.com>
2020-12-29 20:09:31 +0530

    implemented resource sharing page for DWoC

--- frontend/src/components/resource/resources.js
+++ frontend/src/components/resource/resources.js
@@ -1,5 +1,126 @@
 import React from 'react';
-
-const Resources = () => <h1 className='resources'>RESOURCE SHARE</h1>;
-
+import { Button2 } from "../util/button/button";
+import "./resources.css";
+const Resources = () => {
+    return (
+    <div class="resource-section ">
+      <div class="resource-parent">
+        <div class="resource-child child1">
+        <img src="./images/resource.png" alt="" className="resource-image" />
+        </div>
+        <div class="resource-child child2">
+        <h3 class="resource-header-text">Resource Sharing Form</h3>
+          <div class="inside-resource">
+            <div className="resource-input">
+                <label>Name</label><input
+                id="txt_name"
+                type="text"
+                Required="required"
+                name="name"
+              />
+              <i className="fas fa-user"></i>
+            </div>
+            <div className="resource-input">
+              <label>Email ID</label><input
+                id="txt_email"
+                type="text"
+                Required="required"
+                name="email"
+              />
+              <i class="fas fa-envelope-open-text"></i>
+            </div>
+            <div className="resource-input">
+              <label>Phone No.</label><input
+                id="phone"
+                type="tel"
+                Required="required"
+                name="phone"
+              />
+              <i class="fas fa-phone-square-alt"></i>
+            </div>
+            <div className="resource-input">
+              <label>Resource Description</label><textarea
+                id="txt_desc"
+                rows="6"
+                cols="20"
+                name="resourcedesc"
+                Required="required"
+              ></textarea>
+              <i className="fas fa-pen-square"></i>
+            </div>
+            <div className="resource-input">
+              <label>Resource URL</label><input
+                id="txt_link"
+                type="text"
+                Required="required"
+                name="link"
+              />
+              <i class="fas fa-link"></i>
+            </div>
+            <div className="resource-input">
+              <label>Trust Level of Resource</label><br/>
+              <div className="rad">
+              <input
+                id="trust1"
+                class="trust"
+                type="radio"
+                Required="required"
+                name="trustlevel"
+              /><label for="trust1">1</label> 
+              <input
+                id="trust2"
+                class="trust"
+                type="radio"
+                Required="required"
+                name="trustlevel"
+              /><label for="trust2">2</label> 
+              <input
+                id="trust3"
+                class="trust"
+                type="radio"
+                Required="required"
+                name="trustlevel"
+              /><label for="trust3">3</label>
+              <input
+                id="trust4"
+                class="trust"
+                type="radio"
+                Required="required"
+                name="trustlevel"
+              /><label for="trust4">4</label>
+              <input
+                id="trust5"
+                class="trust"
+                type="radio"
+                Required="required"
+                name="trustlevel"
+              /><label for="trust5">5</label> 
+            </div>
+            </div>
+            <div className="resource-input">
+              <label>Valid Until</label><input
+                id="validity"
+                type="date"
+                Required="required"
+                name="dob"
+              />
+            </div>
+            <div className="resource-input">
+              <label>Additional Info (Optional)</label><textarea
+                id="txt_info"
+                rows="6"
+                cols="20"
+                name="info"
+              ></textarea>
+              <i className="far fa-comment-alt"></i>
+            </div>
+            <div style={{ textAlign: "center" }}>
+              <Button2 label="Submit" type="submit" />
+            </div>
+          </div>
+        </div>
+        </div>
+        </div>
+    );
+};
 export default Resources;

f5fccec8fadd247ec4967079cb3c9a9f20c476ab
lakshya Mishra <68530218+kenkirito@users.noreply.github.com>
2021-01-01 03:11:47 +0530

    Update app.js

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/navbar/navbar';
@@ -6,13 +7,12 @@ import Footer from './components/footer/footer';
 import Home from './components/home/home';
 import AboutUs from './components/about/about_us';
 import Broadcasts from './components/broadcast/broadcasts';
+import Broadcasts_Second from './components/broadcast/broadcast_second';
 import Resources from './components/resource/resources';
 import ContactUs from './components/contact/contact_us';
 import Admin from './components/admin/admin';
 import Faqs from './components/faq/faqs';
-import Contact from './components/contact/Design1'
 import NotFound from './components/404/not_found';
-import 'semantic-ui-css/semantic.min.css'
 
 import './app.css';
 
@@ -25,12 +25,14 @@ const App = () => {
           <Switch>
             <Route exact path='/' component={Home} />
             <Route exact path='/about-us' component={AboutUs} />
-            <Route exact path='/broadcasts' component={Broadcasts} />
+            <Route exact path="/broadcasts">
+            <Broadcasts/>
+            <Broadcasts_Second/>
+            </Route>
             <Route exact path='/resources' component={Resources} />
             <Route exact path='/contact-us' component={ContactUs} />
             <Route exact path='/faqs' component={Faqs} />
             <Route exact path='/admin' component={Admin} />
-            <Route exact path='/contact' component={Contact} />
             <Route component={NotFound} />
           </Switch>
         </Router>

26dbe443a6478afd2d43dc5e9c4439019b77fd88
lakshya Mishra <68530218+kenkirito@users.noreply.github.com>
2021-01-01 03:12:52 +0530

    Update broadcasts.js

--- frontend/src/components/broadcast/broadcasts.js
+++ frontend/src/components/broadcast/broadcasts.js
@@ -1,5 +1,158 @@
-import React from 'react';
+import React, { Component } from "react";
+import OwlCarousel from "react-owl-carousel";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
+import "./Owl.css";
+import { withStyles } from '@material-ui/core/styles';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import MuiDialogContent from '@material-ui/core/DialogContent';
+import MuiDialogActions from '@material-ui/core/DialogActions';
+import Dialog from '@material-ui/core/Dialog';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
+import Slide from '@material-ui/core/Slide';
+import Button from '@material-ui/core/Button';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItem from '@material-ui/core/ListItem';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import CloseIcon from '@material-ui/icons/Close';
+import Dialog from 'react-bootstrap-dialog'
 
-const Broadcasts = () => <h1 className='broadcasts'>BROADCASTS</h1>;
+const styles = (theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(2),
+  },
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  },
+});
 
-export default Broadcasts;
+const DialogTitle = withStyles(styles)((props) => {
+  const { children, classes, onClose, ...other } = props;
+  return (
+    <MuiDialogTitle disableTypography className={classes.root} {...other}>
+      <Typography variant="h6">{children}</Typography>
+      {onClose ? (
+        <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </MuiDialogTitle>
+  );
+});
+
+const DialogContent = withStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiDialogContent);
+
+const DialogActions = withStyles((theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(1),
+  },
+}))(MuiDialogActions);
+
+
+const Owlcarsoule=() => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+    return (
+        
+       
+        <div className="Bottom_bgbox">
+        <div className="Bottom_bgbox_first">
+      <div className="motive">
+          <h1 style={{ color: "#121f3a" }}>Recently Broadcast</h1>
+          <div className="dash"></div>
+        </div>
+        </div>
+     
+      <div className="carousel-div">
+      
+        <OwlCarousel
+          items={5}
+          className="owl-theme"
+          loop
+          nav
+          margin={35}
+          autoplay={true}
+        >
+             <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+          <div>
+            <img className="owl-image" alt="" src="./images/help.png" />
+          </div>
+          <div>
+            <img className="owl-image" alt="" src="./images/help.png" />
+          </div>
+          <div>
+            <img className="owl-image" alt="" src="./images/help.png" />
+          </div>
+          <div>
+            <img className="owl-image" alt="" src="./images/aim.png" />
+          </div>
+          <div>
+            <img className="owl-image" alt="" src="./images/outcome.png" />
+          </div>
+          <div>
+            <img className="owl-image" alt="" src="./images/help.png" />
+          </div>
+          <div>
+            <img className="owl-image" alt="" src="./images/about.png" />
+          </div>
+          <div>
+            <img className="owl-image" alt="" src="./images/help.png" />
+          </div>
+          <div>
+            <img className="owl-image" alt="" src="./images/help.png" />
+          </div>
+          
+        ​  </Button>
+      <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
+        <DialogTitle id="customized-dialog-title" onClose={handleClose}>
+          Modal title
+        </DialogTitle>
+        <DialogContent dividers>
+          <Typography gutterBottom>
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+            in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+          </Typography>
+          <Typography gutterBottom>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
+            lacus vel augue laoreet rutrum faucibus dolor auctor.
+          </Typography>
+          <Typography gutterBottom>
+            Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel
+            scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus
+            auctor fringilla.
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose} color="primary">
+            Save changes
+          </Button>
+        </DialogActions>
+      </Dialog>
+      </OwlCarousel>
+        </div>
+     </div>
+    );
+};
+export default Owlcarsoule;

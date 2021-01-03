db779ee72a496dbf4cb6f89f37d0836643dc1cf3
Atharva Upadhye <70993547+atharva-upadhye-01@users.noreply.github.com>
2021-01-03 19:54:17 +0530

    Added getAdminList route at /admin

--- backend/app/routes/admin/index.js
+++ backend/app/routes/admin/index.js
@@ -3,7 +3,9 @@ const validationMiddleware = require('../../../helpers/middlewares/validation');
 const postSuperAdminSchema = require('./@validationSchema');
 const createSuperAdmin = require('./createSuperAdmin');
 const postSuperAdmin = require('./postSuperAdmin');
+const getAdminList = require('./getAdminList');
 
++router.get('/', getAdminList);
 router.get('/createSuperAdmin', createSuperAdmin);
 router.post(
   '/superAdmin',

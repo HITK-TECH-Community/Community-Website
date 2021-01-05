3d9e6d32ea0ec02e97115b87b1daf2e5dd59d082
amanpra123 <amanpra333@gmail.com>
2021-01-05 14:29:16 +0530

    FAQ page UI enhancement

---
+++ frontend/src/components/broadcast/Carousel/Modal/Modals.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
+import "./Modal.css";
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+}));
+
+export default function TransitionsModal(props) {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={props.open}
+        onClose={props.handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={props.open}>
+          <div className="modals">
+            <h1 className="mod_head">Crio Winter Of Doing</h1>
+            <div className="main-cont">
+              <img
+                src="https://images-na.ssl-images-amazon.com/images/I/81A%2BqqYaYFL._SL1280_.jpg"
+                className="image"
+              />
+              <div className="cont">
+                Hello Techies!👋 📢
+                <br />
+                Crio Winter of Doing is here!!🤩🤩 India's top startups are
+                lining up their tech challenges for you!! Join India's largest
+                tech externship program for developers. ✅Work on challenging
+                tech projects (externships) from exciting startups ✅ Interact
+                with the biggest names in India's tech industry ✅ Participate
+                in engaging Masterclass sessions from top engineers in the
+                country ✅Get discovered by Crio's hiring partners Apply now ❗
+                👇 https://www.crio.do/crio-winter-of-doing/ Limited slots are
+                available ❗❗ Thank you, With regards, HITK Tech Community👩‍💻👨‍💻
+                (By HITK students,For HITK students)
+              </div>
+            </div>
+            <h1 className="mod_head">Link</h1>
+          </div>
+        </Fade>
+      </Modal>
+    </div>
+  );
+}

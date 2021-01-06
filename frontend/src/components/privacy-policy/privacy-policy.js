708fbcfd9b30c727a2adf0fa2cbadc91507227f6
Shraddha Gupta <69422250+shraddha-1402@users.noreply.github.com>
2021-01-06 22:01:17 +0530

    Updated image name

--- frontend/src/components/privacy-policy/privacy-policy.js
+++ frontend/src/components/privacy-policy/privacy-policy.js
@@ -5,7 +5,7 @@ const PrivacyPolicy = () => {
   return (
     <div className="pp-section">
       <div className="pp-logo-container">
-        <img className="pp-logo" src="./images/pp.png" />
+        <img className="pp-logo" src="./images/privacy-policy-image.png" />
       </div>
       <div className="pp-heading-container">
         <div className="pp-heading">Privacy Policy</div>

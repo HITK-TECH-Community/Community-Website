28c0810b88b04e480bc6b580496ad54763c90b27
chaitanya <ckhachane67@gmail.com>
2021-01-07 15:15:34 +0530

    added motive data as provided

--- frontend/src/components/home/motive/motive.js
+++ frontend/src/components/home/motive/motive.js
@@ -1,146 +1,183 @@
-import './motive.css';
+import "./motive.css";
 
 const Motive = () => {
   return (
     <div className="motive-div">
-    <div className="container">
-    <div className="row justify-content-center">
-    <div className="col-lg-8">
-      <div className="section-title text-center pb-20">
-      <div className='motive'>
-          <h1>
-            <i className='fas fa-crosshairs'></i>ur Motive
-          </h1>
-          <div className='dash'></div>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-lg-8">
+            <div className="section-title text-center pb-20">
+              <div className="motive">
+                <h1>
+                  <i className="fas fa-crosshairs"></i>Our Motive
+                </h1>
+                <div className="dash"></div>
+              </div>
+              <p className="text text-white">
+                <strong> Motive:</strong>
+                <p>
+                  At HITK Tech Community, we believe there is no limit to
+                  learning. The more you learn, the more places you will go. Our
+                  community aims at:
+                </p>
+                <p>
+                  1. Increasing awareness towards plethora of opportunities such
+                  as internships, competitions, webinars, openings for
+                  collaboration in projects, job openings and contribution in
+                  open source projects.
+                </p>
+                <br></br>
+                <p>
+                  2. All converse together, share information and learn from
+                  each other.
+                </p>
+                <br></br>
+                <p>
+                  3. Helps you accelerate your learning and bring you closer to
+                  like – minded individuals. Ultimately, there will always be a
+                  helping hand at your side.
+                </p>
+                <br></br>
+                <p>
+                  All of the factor counts while making you better, and
+                  ultimately win is an asset. So go ahead and share the vision
+                  for a better future with technology!
+                </p>
+                <br></br>
+              </p>
+            </div>
+          </div>
         </div>
-        <p className="text text-white">
-          Lorem ipsum dolor sit amet, in quodsi vulputate pro. Ius illum
-          vocent mediocritatem reiciendis odit sed, vero amet blanditiis
-          cule dicta iriure at phaedrum.
-        </p>
+        <div className="row justify-content-center">
+          <div className="col-lg-4 col-md-6 col-sm-8 my-3">
+            <div
+              className="single-features d-flex mt-30 wow fadeIn"
+              data-wow-duration="1s"
+              data-wow-delay="0s"
+            >
+              <div className="features-icon">
+                <i className="fab fa-github"></i>
+              </div>
+              <div className="features-content text-left">
+                <h4 className="features-title">
+                  <a href="/#">Great Speakers</a>
+                </h4>
+                <p className="text">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor dunt ut labore et dolore magna aliqua. Ut
+                  enim ad minim veniam.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-6 col-sm-8 my-3">
+            <div
+              className="single-features d-flex mt-30 wow fadeIn"
+              data-wow-duration="1s"
+              data-wow-delay="0.5s"
+            >
+              <div className="features-icon">
+                <i className="fab fa-github"></i>
+              </div>
+              <div className="features-content text-left">
+                <h4 className="features-title">
+                  <a href="/#">New People</a>
+                </h4>
+                <p className="text">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor dunt ut labore et dolore magna aliqua. Ut
+                  enim ad minim veniam.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-6 col-sm-8 my-3">
+            <div
+              className="single-features d-flex mt-30 wow fadeIn"
+              data-wow-duration="1s"
+              data-wow-delay="1s"
+            >
+              <div className="features-icon">
+                <i className="fab fa-github"></i>
+              </div>
+              <div className="features-content text-left">
+                <h4 className="features-title">
+                  <a href="/#">Global Event</a>
+                </h4>
+                <p className="text">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor dunt ut labore et dolore magna aliqua. Ut
+                  enim ad minim veniam.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-6 col-sm-8 my-3">
+            <div
+              className="single-features d-flex mt-30 wow fadeIn"
+              data-wow-duration="1s"
+              data-wow-delay="0.5s"
+            >
+              <div className="features-icon">
+                <i className="fab fa-github"></i>
+              </div>
+              <div className="features-content text-left">
+                <h4 className="features-title">
+                  <a href="/#">Get Inspired</a>
+                </h4>
+                <p className="text">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor dunt ut labore et dolore magna aliqua. Ut
+                  enim ad minim veniam.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-6 col-sm-8 my-3">
+            <div
+              className="single-features d-flex mt-30 wow fadeIn"
+              data-wow-duration="1s"
+              data-wow-delay="1s"
+            >
+              <div className="features-icon">
+                <i className="fab fa-github"></i>
+              </div>
+              <div className="features-content text-left">
+                <h4 className="features-title">
+                  <a href="/#">Networking Session</a>
+                </h4>
+                <p className="text">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor dunt ut labore et dolore magna aliqua. Ut
+                  enim ad minim veniam.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-6 col-sm-8 my-3">
+            <div
+              className="single-features d-flex mt-30 wow fadeIn"
+              data-wow-duration="1s"
+              data-wow-delay="1.5s"
+            >
+              <div className="features-icon">
+                <i className="fab fa-github"></i>
+              </div>
+              <div className="features-content text-left">
+                <h4 className="features-title">
+                  <a href="/#">Meet New Faces</a>
+                </h4>
+                <p className="text">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor dunt ut labore et dolore magna aliqua. Ut
+                  enim ad minim veniam.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-  <div className="row justify-content-center">
-    <div className="col-lg-4 col-md-6 col-sm-8 my-3">
-      <div
-        className="single-features d-flex mt-30 wow fadeIn"
-        data-wow-duration="1s"
-        data-wow-delay="0s"
-      >
-        <div className="features-icon">
-          <i className="fab fa-github"></i>
-        </div>
-        <div className="features-content text-left">
-          <h4 className="features-title"><a href="/#">Great Speakers</a></h4>
-          <p className="text">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-            do eiusmod tempor dunt ut labore et dolore magna aliqua. Ut
-            enim ad minim veniam.
-          </p>
-        </div>
-    </div>
-    </div>
-    <div className="col-lg-4 col-md-6 col-sm-8 my-3">
-      <div
-        className="single-features d-flex mt-30 wow fadeIn"
-        data-wow-duration="1s"
-        data-wow-delay="0.5s"
-      >
-        <div className="features-icon">
-          <i className="fab fa-github"></i>
-        </div>
-        <div className="features-content text-left">
-          <h4 className="features-title"><a href="/#">New People</a></h4>
-          <p className="text">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-            do eiusmod tempor dunt ut labore et dolore magna aliqua. Ut
-            enim ad minim veniam.
-          </p>
-        </div>
-      </div>
-    </div>
-    <div className="col-lg-4 col-md-6 col-sm-8 my-3">
-      <div
-        className="single-features d-flex mt-30 wow fadeIn"
-        data-wow-duration="1s"
-        data-wow-delay="1s"
-      >
-        <div className="features-icon">
-          <i className="fab fa-github"></i>
-        </div>
-        <div className="features-content text-left">
-          <h4 className="features-title"><a href="/#">Global Event</a></h4>
-          <p className="text">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-            do eiusmod tempor dunt ut labore et dolore magna aliqua. Ut
-            enim ad minim veniam.
-          </p>
-        </div>
-      </div>
-    </div>
-    <div className="col-lg-4 col-md-6 col-sm-8 my-3">
-      <div
-        className="single-features d-flex mt-30 wow fadeIn"
-        data-wow-duration="1s"
-        data-wow-delay="0.5s"
-      >
-        <div className="features-icon">
-          <i className="fab fa-github"></i>
-        </div>
-        <div className="features-content text-left">
-          <h4 className="features-title"><a href="/#">Get Inspired</a></h4>
-          <p className="text">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-            do eiusmod tempor dunt ut labore et dolore magna aliqua. Ut
-            enim ad minim veniam.
-          </p>
-        </div>
-      </div>
-    </div>
-    <div className="col-lg-4 col-md-6 col-sm-8 my-3">
-      <div
-        className="single-features d-flex mt-30 wow fadeIn"
-        data-wow-duration="1s"
-        data-wow-delay="1s"
-      >
-        <div className="features-icon">
-          <i className="fab fa-github"></i>
-        </div>
-        <div className="features-content text-left">
-          <h4 className="features-title">
-            <a href="/#">Networking Session</a>
-          </h4>
-          <p className="text">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-            do eiusmod tempor dunt ut labore et dolore magna aliqua. Ut
-            enim ad minim veniam.
-          </p>
-        </div>
-      </div>
-    </div>
-    <div className="col-lg-4 col-md-6 col-sm-8 my-3">
-      <div
-        className="single-features d-flex mt-30 wow fadeIn"
-        data-wow-duration="1s"
-        data-wow-delay="1.5s"
-      >
-        <div className="features-icon">
-          <i className="fab fa-github"></i>
-        </div>
-        <div className="features-content text-left">
-          <h4 className="features-title"><a href="/#">Meet New Faces</a></h4>
-          <p className="text">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-            do eiusmod tempor dunt ut labore et dolore magna aliqua. Ut
-            enim ad minim veniam.
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
-  </div>
-  </div>
   );
 };
 

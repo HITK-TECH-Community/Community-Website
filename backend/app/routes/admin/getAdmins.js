337c736c1f900b59b6abcb43f125190b13ab7a25
Rohit Agarwal <rohitagrl1302@gmail.com>
2021-01-07 16:48:40 +0530

    feat(backend): PR Review final changes
    modifies code structure such that it is free from linter errors.
    @atharva-upadhye-01 Please go through this once.

--- backend/app/routes/admin/getAdmins.js
+++ backend/app/routes/admin/getAdmins.js
@@ -3,8 +3,9 @@ const Admin = require('../../models/Admin');
 const { ErrorHandler } = require('../../../helpers/error');
 const constants = require('../../../constants');
 
-const getAdminsAggregate = (adminType, page) => {
+const getAdminsAggregate = (match, page) => {
   const pipeline = [
+    { $match: match },
     {
       $project: {
         _id: 0,
@@ -16,33 +17,29 @@ const getAdminsAggregate = (adminType, page) => {
         isSuperAdmin: 1,
       },
     },
-  ];
-  if (adminType === 'superAdmin' || adminType === 'admin') {
-    pipeline.unshift({
-      $match: {
-        isSuperAdmin: adminType === 'superAdmin',
-      },
-    });
-  }
-  if (page === undefined) page = 1;
-  pipeline.push(
     { $skip: constants.PAGINATION_LIMIT.GET_ADMINS * (Number(page) - 1) },
-    { $limit: constants.PAGINATION_LIMIT.GET_ADMINS }
-  );
+    { $limit: constants.PAGINATION_LIMIT.GET_ADMINS },
+  ];
   return pipeline;
 };
 
 module.exports = async (req, res, next) => {
-  const options = req.query;
-  const { page, type: adminType } = options;
-
+  const page = req.query.page || 1;
+  const adminType = req.query.type;
+  let match = {};
+  if (adminType) {
+    match = {
+      isSuperAdmin: adminType === 'superAdmin',
+    };
+  }
   const [err, response] = await to(
-    Admin.aggregate(getAdminsAggregate(adminType, page))
+    Admin.aggregate(getAdminsAggregate(match, page))
   );
   if (err) {
     const error = new ErrorHandler(constants.ERRORS.DATABASE, {
       statusCode: '500',
-      message: 'The server encountered an unexpected condition which prevented it from fulfilling the request.',
+      message:
+        'The server encountered an unexpected condition which prevented it from fulfilling the request.',
       errorStack: err,
     });
     return next(error);

ee1f9b1f1ca0c39293cf8294adaf418bdd188dd9
Aman Prateek <42955585+amanprateek123@users.noreply.github.com>
2021-01-09 14:35:07 +0530

    Broadcast Page Design (#206)
    * FAQ page UI enhancement
    
    * FAQ page updates
    
    * Carousel Added in broadcast page
    
    * Pop-up added in broadcast page
    
    * FAQ page UI enhancement
    
    * FAQ page updates
    
    * Carousel Added in broadcast page
    
    * Pop-up added in broadcast page
    
    * conflict solved

--- frontend/src/components/broadcast/broadcasts.js
+++ frontend/src/components/broadcast/broadcasts.js
@@ -1,90 +1,18 @@
-import React, { Component } from "react";
-import OwlCarousel from "react-owl-carousel";
-import "owl.carousel/dist/assets/owl.carousel.css";
-import "owl.carousel/dist/assets/owl.theme.default.css";
-import "./owl.css";
-import Card from "./card.js";
+import React from "react";
+import "./broadcast.css";
+import Carousel from "./Carousel/Carousel";
 
-export class Owldemo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      options: {
-        loop: true,
-        margin: 15,
-        responsive: {
-          0: {
-            nav: false,
-            items: 1,
-          },
-          497: {
-            nav: false,
-            items: 2,
-          },
-          551: {
-            nav: false,
-            items: 3,
-            margin:125,
-          },
-          800: {
-            nav: false,
-            items: 3,
-            margin: 0,
-          },
-          1000: {
-            nav: true,
-            items: 4,
-            margin: 50,
-          },
-          1100: {
-            nav: true,
-            items: 5,
-          },
-        },
-      },
-    };
-  }
-  render() {
-    return (
-      <div className="Bottom_bgbox">
-        <div className="Bottom_bgbox_first">
-          <div className="motive">
-            <h1 style={{ color: "#1B2431" }}>Previously Broadcast</h1>
-            <div className="dash"></div>
-          </div>
-        </div>
-
-        <div className="carousel-div">
-          <OwlCarousel
-            className="owl-theme"
-            {...this.state.options}
-            autoplay={true}
-            responsiveClass={true}
-
-          >
-            <div>
-              <Card />
-            </div>
-            <div>
-              <Card />
-            </div>
-            <div>
-              <Card />
-            </div>
-            <div>
-              <Card />
-            </div>
-            <div>
-              <Card />
-            </div>
-            <div>
-              <Card />
-            </div>
-          </OwlCarousel>
-        </div>
+const Broadcasts = () => {
+  return (
+    <div>
+      <div>
+        <Carousel head="Recent Broadcasts" />
       </div>
-    );
-  }
-}
+      <div>
+        <Carousel head="Previous Broadcasts" />
+      </div>
+    </div>
+  );
+};
 
-export default Owldemo;
+export default Broadcasts;

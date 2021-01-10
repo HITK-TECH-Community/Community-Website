70868d47ec5780c929a8b59cf0a04d87ad587e09
Satya Prakash Satyam <satty9509@gmail.com>
2021-01-10 22:28:53 +0530

    issue #94

--- backend/app/routes/admin/index.js
+++ backend/app/routes/admin/index.js
@@ -5,11 +5,13 @@ const { authMiddleware } = require('../../../helpers/middlewares/auth');
 const {
   postSuperAdminSchema,
   getAdminsSchema,
+  passwordChangeSchema,
 } = require('./@validationSchema');
 const createSuperAdmin = require('./createSuperAdmin');
 const postSuperAdmin = require('./postSuperAdmin');
 const postAdmin = require('./postAdmin');
 const getAdmins = require('./getAdmins');
+const changePassword = require('./changePassword');
 
 router.get(
   '/',
@@ -28,6 +30,13 @@ router.post(
   validationMiddleware(postSuperAdminSchema),
   authMiddleware,
   postAdmin
+);
+
+router.put(
+  '/password',
+  validationMiddleware(passwordChangeSchema),
+  authMiddleware,
+  changePassword
 );
 
 module.exports = router;

f2fee1c6fbb13e1cf2f6ff01ab44b567d39512d9
satya prakash satyam <61646176+N00Bmaster2810@users.noreply.github.com>
2021-01-10 18:14:03 +0530

    Created POST route for adding admins (#201)
    * issue #79
    
    * Made suggested changes
    
    * linting fixed
    
    * changes made
    
    * adds superAdmin field to jwt to save mongoDB call
    
    Co-authored-by: Rohit Agarwal <rohitagrl1302@gmail.com>

--- backend/app/routes/admin/index.js
+++ backend/app/routes/admin/index.js
@@ -1,12 +1,14 @@
 const router = require('express').Router({ mergeParams: true });
 const validationMiddleware = require('../../../helpers/middlewares/validation');
 const { authMiddleware } = require('../../../helpers/middlewares/auth');
+
 const {
   postSuperAdminSchema,
   getAdminsSchema,
 } = require('./@validationSchema');
 const createSuperAdmin = require('./createSuperAdmin');
 const postSuperAdmin = require('./postSuperAdmin');
+const postAdmin = require('./postAdmin');
 const getAdmins = require('./getAdmins');
 
 router.get(
@@ -20,6 +22,12 @@ router.post(
   '/superAdmin',
   validationMiddleware(postSuperAdminSchema),
   postSuperAdmin
+);
+router.post(
+  '/',
+  validationMiddleware(postSuperAdminSchema),
+  authMiddleware,
+  postAdmin
 );
 
 module.exports = router;

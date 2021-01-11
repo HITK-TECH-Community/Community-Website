f6d45bbcb3aeb7af1e01fb37d03c140658c5343c
kajol <kajolkumarisingh222@gmail.com>
2021-01-11 21:06:03 +0530

    motive_fix

--- frontend/src/components/home/motive/motive.js
+++ frontend/src/components/home/motive/motive.js
@@ -1,4 +1,4 @@
-import './motive.css';
+import "./motive.css";
 
 const Motive = () => {
   return (
@@ -7,15 +7,17 @@ const Motive = () => {
         <div className="row justify-content-center">
           <div className="col-lg-8">
             <div className="section-title text-center pb-20">
-              <div className='motive'>
+              <div className="motive">
                 <h1>
-                  <i className='fas fa-crosshairs'></i>ur Motive
-          </h1>
-                <div className='dash'></div>
+                  <i className="fas fa-crosshairs"></i>ur Motive
+                </h1>
+                <div className="dash"></div>
               </div>
               <p className="text text-white my-3">
-                At HITK Tech Community, we believe there is no limit to learning. The more you learn, the more places you will go. Our community aims at:
-        </p>
+                At HITK Tech Community, we believe there is no limit to
+                learning. The more you learn, the more places you will go. Our
+                community aims at:
+              </p>
             </div>
           </div>
         </div>
@@ -30,10 +32,14 @@ const Motive = () => {
                 <i className="fas fa-microphone"></i>
               </div>
               <div className="features-content text-left">
-                <h4 className="features-title"><a href="/#">Great Speakers</a></h4>
-                <p className="">
-                  Increasing awareness towards plethora of opportunities such as internships, competitions, webinars, openings for collaboration in projects, job openings and contribution in open source projects.
-               </p>
+                <h4 className="features-title">
+                  <a href="/#">Increase Awareness</a>
+                </h4>
+                <p className="motive-content">
+                  Towards plethora of opportunities such as internships,
+                  competitions, openings for collaboration in projects, job
+                  openings and contribution in open source projects.
+                </p>
               </div>
             </div>
           </div>
@@ -47,10 +53,13 @@ const Motive = () => {
                 <i className="fa fa-users"></i>
               </div>
               <div className="features-content text-left">
-                <h4 className="features-title"><a href="/#">New People</a></h4>
-                <p className="">
-                  All converse together, share information and learn from each other.
-          </p>
+                <h4 className="features-title">
+                  <a href="/#">Tech Community</a>
+                </h4>
+                <p className="motive-content">
+                  Forming a global tech community where developers can converse
+                  together, share information and learn from each other.
+                </p>
               </div>
             </div>
           </div>
@@ -64,20 +73,25 @@ const Motive = () => {
                 <i className="fas fa-globe"></i>
               </div>
               <div className="features-content text-left">
-                <h4 className="features-title"><a href="/#">Global Event</a></h4>
-                <p className="">
-                  Helps you accelerate your learning and bring you closer to like – minded individuals. Ultimately, there will always be a helping hand at your side.
-          </p>
+                <h4 className="features-title">
+                  <a href="/#">A Lot of Learning</a>
+                </h4>
+                <p className="motive-content">
+                  Helping you accelerate your learning and bring you closer to
+                  like – minded individuals. Ultimately, there will always be a
+                  helping hand at your side.
+                </p>
               </div>
             </div>
           </div>
           <p className="text text-white my-3 text-center">
-            All of the factor counts while making you better, and ultimately win is an asset. So go ahead and share the vision for a better future with technology!
-        </p>
+            All of the factor counts while making you better, and ultimately win
+            is an asset. So go ahead and share the vision for a better future
+            with technology!
+          </p>
         </div>
       </div>
     </div>
-
   );
 };
 export default Motive;

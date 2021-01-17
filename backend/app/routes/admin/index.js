33d5dd488cac552608863e5dfe7463712075c025
Rohit Agarwal <36669463+jackfrost13@users.noreply.github.com>
2021-01-17 16:06:39 +0530

    Revert "issue #94 (#244)"
    This reverts commit 8bfffc7b0a99eac6317297ea264a97bab047a614.

--- backend/app/routes/admin/index.js
+++ backend/app/routes/admin/index.js
@@ -5,13 +5,11 @@ const { authMiddleware } = require('../../../helpers/middlewares/auth');
 const {
   postSuperAdminSchema,
   getAdminsSchema,
-  passwordChangeSchema,
 } = require('./@validationSchema');
 const createSuperAdmin = require('./createSuperAdmin');
 const postSuperAdmin = require('./postSuperAdmin');
 const postAdmin = require('./postAdmin');
 const getAdmins = require('./getAdmins');
-const changePassword = require('./changePassword');
 
 router.get(
   '/',
@@ -30,13 +28,6 @@ router.post(
   validationMiddleware(postSuperAdminSchema),
   authMiddleware,
   postAdmin
-);
-
-router.put(
-  '/password',
-  validationMiddleware(passwordChangeSchema),
-  authMiddleware,
-  changePassword
 );
 
 module.exports = router;

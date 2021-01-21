6bacee2ceb31d5627ea984b5ec951381a17726aa
Vedika Agarwal <vedika.ag99@gmail.com>
2021-01-21 19:42:25 +0530

    carousel put inside bootstrap container fr DWoC (#282)

--- frontend/src/components/home/carousel/carousel.js
+++ frontend/src/components/home/carousel/carousel.js
@@ -42,25 +42,27 @@ export class Owldemo1 extends Component {
           <h1 style={{ color: "#121f3a" }}>Curious to know More about Us?</h1>
           <div className="dash"></div>
         </div>
-        <OwlCarousel
-          className="owl-theme"
-          {...this.state.options}
-          autoplay={true}
-          responsiveClass={true}
-        >
-          <div>
-            <img className="owl-image" alt="" src="./images/help.png" />
-          </div>
-          <div>
-            <img className="owl-image" alt="" src="./images/aim.png" />
-          </div>
-          <div>
-            <img className="owl-image" alt="" src="./images/outcome.png" />
-          </div>
-          <div>
-            <img className="owl-image" alt="" src="./images/about.png" />
-          </div>
-        </OwlCarousel>
+        <div className="container">
+          <OwlCarousel
+            className="owl-theme"
+            {...this.state.options}
+            autoplay={true}
+            responsiveClass={true}
+          >
+            <div>
+              <img className="owl-image" alt="" src="./images/help.png" />
+            </div>
+            <div>
+              <img className="owl-image" alt="" src="./images/aim.png" />
+            </div>
+            <div>
+              <img className="owl-image" alt="" src="./images/outcome.png" />
+            </div>
+            <div>
+              <img className="owl-image" alt="" src="./images/about.png" />
+            </div>
+          </OwlCarousel>
+        </div>
       </div>
     );
   }

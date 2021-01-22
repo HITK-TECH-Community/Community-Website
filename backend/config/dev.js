c5ce5b1335bde5af23fd798c3d04e5f9f5a23ff3
Piyush Dhamane <piyush.dhamne@gmail.com>
2021-01-22 21:05:57 +0530

    Revised Second phase changes

--- backend/config/dev.js
+++ backend/config/dev.js
@@ -3,8 +3,13 @@ module.exports = {
   "smtpOptions" : {
   "service": "Gmail",
   "auth": {
+<<<<<<< HEAD
     "user": '[EMAIL ID HERE]',
     "pass": '[PASSOWORD]'
+=======
+    "user": '[EMAIL]',
+    "pass": '[PASSWORD]'
+>>>>>>> d5a3059... Revised Second phase changes
   },
   "tls": {
     "rejectUnauthorized": false

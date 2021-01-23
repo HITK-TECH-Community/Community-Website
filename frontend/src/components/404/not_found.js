06a47339d153adc55f2e45e68299ba31a0e133de
Vedika Agarwal <67699840+vedikaag99@users.noreply.github.com>
2021-01-23 21:29:16 +0530

    changes made

--- frontend/src/components/404/not_found.js
+++ frontend/src/components/404/not_found.js
@@ -1,19 +1,24 @@
 import React from "react";
 import "./not_found.css";
+import { Button2 } from "../util/button/button";
 
 const NotFound = () => {
   return (
-    <div className="error">
-      <div className="pp-logo-container">
-        <img className="error-logo" src="./images/error.png" alt="404" />
+    <div className="not_found">
+      <div className="error">
+        <div className="pp-logo-container">
+          <img className="error-logo" src="./images/error.png" alt="404" />
+        </div>
+        <p className="error-text">
+          Looks like you're lost!
+        </p>
+        <div className="error-content">
+            The page you are looking for is not available.
+        </div>
+        <a href="/">
+          <Button2 label="Go Home!" type="submit" />
+        </a>
       </div>
-      <p className="error-text">
-        Looks like you're lost!
-      </p>
-      <div className="error-content">
-          The page you are looking for is not available.
-      </div>
-      <a href="/" class="back"><i class="fas fa-arrow-left"></i> Go Home!</a>
     </div>
   );
 };

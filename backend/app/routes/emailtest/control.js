7cb83fad02e2edf97d4acc66c46375ae1b6fb8bb
Piyush Dhamane <piyush.dhamne@gmail.com>
2021-01-25 20:26:45 +0530

    Third Phase Changes

--- backend/app/routes/emailtest/control.js
+++ backend/app/routes/emailtest/control.js
@@ -1,11 +1,19 @@
-const service = require('./emailservice');
+const to = require('await-to-js').default;
+const sendEmail = require('../../../helpers/mailer');
 const router = require('express').Router({ mergeParams: true });
 router.post('/', email);
 
 async function email(req, res, next) {
-    await service.sendVerificationEmail(req.body.toEmail, req.body.fromEmail)
-        .then(() => res.json({message: `Email Sent!`}))
-        .catch(next);
+    [err] = await to (sendEmail({
+        from : req.body.fromEmail,
+        to: req.body.toEmail
+    })
+    );
+    if(err){
+        return next(err);
+    }
+    res.json({message: `Email Sent!`});
+    return next();
 }
 
 module.exports = router;
--- backend/helpers/mailer.js
+++ backend/helpers/mailer.js
@@ -1,16 +1,10 @@
 const nodemailer = require("nodemailer");
-const config = require("../config/dev");
+const config = require("../config");
 const templates = require("./sendmail")
 
 module.exports = sendEmail;
 
 async function sendEmail({ to, subject = templates.INVITE_ADMIN.SUBJECT, html = templates.INVITE_ADMIN.BODY, from}) {
     const transporter = nodemailer.createTransport(config.smtpOptions);
-    await transporter.sendMail({ from, to, subject, html},function(error, info){
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ');
-        }
-      });
+    await transporter.sendMail({ from, to, subject, html});
 }

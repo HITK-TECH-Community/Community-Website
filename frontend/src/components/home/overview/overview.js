47229274542a35beb90b654c21c91efbbe2a1c77
sakshikhachane <khachanesakshi.1112@gmail.com>
2021-01-27 12:00:57 +0530

    Added bootstrap container for hero section

--- frontend/src/components/home/overview/overview.js
+++ frontend/src/components/home/overview/overview.js
@@ -4,17 +4,27 @@ import "./overview.css";
 const Overview = () => {
   return (
     <div className="content">
-      <div className="overview">
-        <div>
-          <h1>HITK Tech Community</h1>
-          <div className="dash"></div>
-          <p>
-            A community driven by tech enthusiasts and open-source contributors,
-            to help students of HITK to be part of the open-source ecosystem by
-            providing right resources at right time.
-          </p>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-lg-10">
+            <div className="overview">
+              <div>
+                <h1>HITK Tech Community</h1>
+                <div className="dash"></div>
+                <p>
+                  A community driven by tech enthusiasts and open-source
+                  contributors, to help students of HITK to be part of the
+                  open-source ecosystem by providing right resources at right
+                  time.
+                </p>
+              </div>
+              <img
+                src="./images/logo.png"
+                alt="HITK Tech Community Official Logo"
+              />
+            </div>
+          </div>
         </div>
-        <img src="./images/logo.png" alt="HITK Tech Community Official Logo" />
       </div>
     </div>
   );

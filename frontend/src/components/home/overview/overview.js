8c95826991ef95b68c8b809ab0bfe678d6a78319
Swastik Raj Singh <71871571+Swastik1710@users.noreply.github.com>
2021-01-28 13:40:54 +0530

    Hero section changes (#305)
    * Hero changes
    
    * Minor Changes
    
    * Minor

--- frontend/src/components/home/overview/overview.js
+++ frontend/src/components/home/overview/overview.js
@@ -3,18 +3,26 @@ import "./overview.css";
 
 const Overview = () => {
   return (
-    <div className="content">
-      <div className="overview">
-        <div>
-          <h1>HITK Tech Community</h1>
-          <div className="dash"></div>
-          <p>
-            A community driven by tech enthusiasts and open-source contributors,
-            to help students of HITK to be part of the open-source ecosystem by
-            providing right resources at right time.
-          </p>
+    <div className="content container">
+      <div className="row justify-content-center">
+        <div className="col-12">
+          <div className="overview">
+            <div>
+              <h1>HITK Tech Community</h1>
+              <div className="dash"></div>
+              <p>
+                A community driven by tech enthusiasts and open-source
+                contributors, to help students of HITK to be part of the
+                open-source ecosystem by providing right resources at right
+                time.
+              </p>
+            </div>
+            <img
+              src="./images/logo.png"
+              alt="HITK Tech Community Official Logo"
+            />
+          </div>
         </div>
-        <img src="./images/logo.png" alt="HITK Tech Community Official Logo" />
       </div>
     </div>
   );

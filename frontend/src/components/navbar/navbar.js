4912146b88f83376b7ae77efb5eb863214ac24b1
kajol <kajolkumarisingh222@gmail.com>
2021-02-03 13:09:16 +0530

    ♻️ Community Logo Added

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
     <Fragment>
       <nav className="navbar-div">
         <NavLink to="/" className="navbar-logo">
-          HITK
+          <img src="./community-logo.png" alt="logo" width="40px" height="40px"/>
         </NavLink>
         <div className="menu-icon" onClick={toggleNav}>
           <i className={isNavOpen ? "fas fa-times" : "fas fa-bars"}></i>

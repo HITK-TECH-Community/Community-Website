c9ad1b1363ea311b6b12f287bb6394709c6cfcec
Neha Parmar <nehaparmar1899@gmail.com>
2021-02-08 19:21:50 +0530

    css modules

--- frontend/src/components/admin/broadcast/broadcast.js
+++ frontend/src/components/admin/broadcast/broadcast.js
@@ -1,25 +1,22 @@
 import React from "react";
-import "./broadcast.css";
+import styles from "./broadcast.module.css";
 import { AiFillEdit } from "react-icons/ai";
 import { AiOutlinePlus } from "react-icons/ai";
 import { Link } from "react-router-dom";
 export default function Broadcast() {
-  // const openEditor = () => {
-  //   alert("hello");
-  // };
   return (
-    <div className="broadcast">
+    <div className={styles.broadcast}>
       <h1 style={{ textAlign: "center" }}> Broadcasts </h1>
-      <div className="cards">
-        <div className="carditem">
-          <div className="clickable-card">
-            <div className="card-title">
+      <div className={styles.cards}>
+        <div className={styles.carditem}>
+          <div className={styles.clickablecard}>
+            <div className={styles.cardtitle}>
               <Link to="/add_broadcast" style={{ color: "white" }}>
                 ADD BROADCAST
               </Link>
-              <AiOutlinePlus className="add" />
+              <AiOutlinePlus className={styles.add} />
             </div>
-            <div className="card-content">
+            <div className={styles.cardcontent}>
               <p style={{ textAlign: "left" }}>To add a new broadcast</p>
               <p
                 // onClick={openEditor}
@@ -32,14 +29,14 @@ export default function Broadcast() {
             </div>
           </div>
         </div>
-        <div className="carditem">
-          <div className="clickable-card">
-            <div className="card-title">
+        <div className={styles.carditem}>
+          <div className={styles.clickablecard}>
+            <div className={styles.cardtitle}>
               MANAGE BROADCASTS
-              <AiFillEdit className="editt" />
+              <AiFillEdit className={styles.editt} />
             </div>
-            <div className="card-content">
-              <button className="main-btn" type="submit">
+            <div className={styles.cardcontent}>
+              <button className={styles.mainbtn} type="submit">
                 Manage here
               </button>
             </div>

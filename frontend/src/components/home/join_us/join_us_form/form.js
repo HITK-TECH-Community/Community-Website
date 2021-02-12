de0ac8f50bf0051030da72b33736a026af3ff3b8
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2021-02-12 14:06:22 +0530

    🔑 fix: password Hide and Show button now works on the first go!😁😁 (#379)

--- frontend/src/components/home/join_us/join_us_form/form.js
+++ frontend/src/components/home/join_us/join_us_form/form.js
@@ -46,7 +46,9 @@ const JoinUsForm = () => {
                   Required="required"
                   name="email"
                 />
-                <i className={`fas fa-envelope-open-text ${styles.envelope}`}></i>
+                <i
+                  className={`fas fa-envelope-open-text ${styles.envelope}`}
+                ></i>
               </div>
             </div>
             <div className={styles.formGroup}>
@@ -75,126 +77,116 @@ const JoinUsForm = () => {
             <div className={styles.joinusformInput1}>
               <label className={`mb-4 ${styles.ID}`}>Interested Domains</label>
               <div className={styles.checkbuttons}>
-              <div class="row">
-              <div class="col-xl-6">
-                <div className={`${styles.form_check} form-check-inline ${styles.checkbox_item}`}>
-                  <input
-                      name="ml"
-                      type="checkbox"
-                    />
-                  <label className={`${styles.mx_3} ${styles.label}`}>
-                    Machine Learning
-                  </label>
+                <div class="row">
+                  <div class="col-xl-6">
+                    <div
+                      className={`${styles.form_check} form-check-inline ${styles.checkbox_item}`}
+                    >
+                      <input name="ml" type="checkbox" />
+                      <label className={`${styles.mx_3} ${styles.label}`}>
+                        Machine Learning
+                      </label>
+                    </div>
+                  </div>
+                  <div class="col-xl-6">
+                    <div
+                      className={`${styles.form_check} form-check-inline ${styles.checkbox_item}`}
+                    >
+                      <input name="ai" type="checkbox" />
+                      <label className={`${styles.mx_3} ${styles.label}`}>
+                        Artificial Intelligence
+                      </label>
+                    </div>
+                  </div>
                 </div>
+                <div class="row">
+                  <div class="col-xl-6">
+                    <div
+                      className={`${styles.form_check} form-check-inline ${styles.checkbox_item}`}
+                    >
+                      <input name="android" type="checkbox" />
+                      <label className={`${styles.mx_3} ${styles.label}`}>
+                        Android
+                      </label>
+                    </div>
+                  </div>
+                  <div class="col-xl-6">
+                    <div
+                      className={`${styles.form_check} form-check-inline ${styles.checkbox_item}`}
+                    >
+                      <input name="web" type="checkbox" />
+                      <label className={`${styles.mx_3} ${styles.label}`}>
+                        Web Development
+                      </label>
+                    </div>
+                  </div>
                 </div>
-                <div class="col-xl-6">
-                <div className={`${styles.form_check} form-check-inline ${styles.checkbox_item}`}>
-                  <input
-                      name="ai"
-                      type="checkbox"
-                    />
-                  <label className={`${styles.mx_3} ${styles.label}`}>
-                    Artificial Intelligence
-                  </label>
+                <div class="row">
+                  <div class="col-xl-6">
+                    <div
+                      className={`${styles.form_check} form-check-inline ${styles.checkbox_item}`}
+                    >
+                      <input name="poster" type="checkbox" />
+                      <label className={`${styles.mx_3} ${styles.label}`}>
+                        Poster Designing
+                      </label>
+                    </div>
+                  </div>
+                  <div class="col-xl-6">
+                    <div
+                      className={`${styles.form_check} form-check-inline ${styles.checkbox_item}`}
+                    >
+                      <input name="video" type="checkbox" />
+                      <label className={`${styles.mx_3} ${styles.label}`}>
+                        Video Editing
+                      </label>
+                    </div>
+                  </div>
                 </div>
+                <div class="row">
+                  <div class="col-xl-6">
+                    <div
+                      className={`${styles.form_check} form-check-inline ${styles.checkbox_item}`}
+                    >
+                      <input name="social" type="checkbox" />
+                      <label className={`${styles.mx_3} ${styles.label}`}>
+                        Social Media Handler
+                      </label>
+                    </div>
+                  </div>
+                  <div class="col-xl-6">
+                    <div
+                      className={`${styles.form_check} form-check-inline ${styles.checkbox_item}`}
+                    >
+                      <input name="iot" type="checkbox" />
+                      <label className={`${styles.mx_3} ${styles.label}`}>
+                        IoT
+                      </label>
+                    </div>
+                  </div>
                 </div>
-              </div>
-              <div class="row">
-                <div class="col-xl-6">
-                <div className={`${styles.form_check} form-check-inline ${styles.checkbox_item}`}>
-                  <input
-                      name="android"
-                      type="checkbox"
-                    />
-                  <label className={`${styles.mx_3} ${styles.label}`}>
-                      Android
-                  </label>
+                <div class="row">
+                  <div class="col-xl-6">
+                    <div
+                      className={`${styles.form_check} form-check-inline ${styles.checkbox_item}`}
+                    >
+                      <input name="content" type="checkbox" />
+                      <label className={`${styles.mx_3} ${styles.label}`}>
+                        Content Writing
+                      </label>
+                    </div>
+                  </div>
+                  <div class="col-xl-6">
+                    <div
+                      className={`${styles.form_check} form-check-inline ${styles.checkbox_item}`}
+                    >
+                      <input name="other" type="checkbox" />
+                      <label className={`${styles.mx_3} ${styles.label}`}>
+                        Other
+                      </label>
+                    </div>
+                  </div>
                 </div>
-                </div>
-                <div class="col-xl-6">
-                <div className={`${styles.form_check} form-check-inline ${styles.checkbox_item}`}>
-                  <input
-                      name="web"
-                      type="checkbox"
-                    />
-                  <label className={`${styles.mx_3} ${styles.label}`}>
-                    Web Development
-                  </label>
-                </div>
-                </div>
-              </div>
-              <div class="row">
-              <div class="col-xl-6">
-              <div className={`${styles.form_check} form-check-inline ${styles.checkbox_item}`}>
-                  <input
-                      name="poster"
-                      type="checkbox"
-                    />
-                  <label className={`${styles.mx_3} ${styles.label}`}>
-                    Poster Designing
-                  </label>
-                </div>
-                </div>
-                <div class="col-xl-6">
-                <div className={`${styles.form_check} form-check-inline ${styles.checkbox_item}`}>
-                  <input
-                      name="video"
-                      type="checkbox"
-                    />
-                  <label className={`${styles.mx_3} ${styles.label}`}>
-                    Video Editing
-                  </label>
-                </div>
-                </div>
-              </div>
-              <div class="row">
-              <div class="col-xl-6">
-                <div className={`${styles.form_check} form-check-inline ${styles.checkbox_item}`}>
-                  <input
-                      name="social"
-                      type="checkbox"
-                    />
-                  <label className={`${styles.mx_3} ${styles.label}`}>
-                    Social Media Handler
-                  </label>
-                </div>
-                </div>
-                <div class="col-xl-6">
-                <div className={`${styles.form_check} form-check-inline ${styles.checkbox_item}`}>
-                    <input
-                        name="iot"
-                        type="checkbox"
-                      />
-                    <label className={`${styles.mx_3} ${styles.label}`}>
-                      IoT
-                    </label>
-                </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-xl-6">
-                <div className={`${styles.form_check} form-check-inline ${styles.checkbox_item}`}>
-                  <input
-                      name="content"
-                      type="checkbox"
-                    />
-                  <label className={`${styles.mx_3} ${styles.label}`}>
-                    Content Writing
-                  </label>
-                </div>
-                </div>
-                <div class="col-xl-6">
-                <div className={`${styles.form_check} form-check-inline ${styles.checkbox_item}`}>
-                  <input
-                      name="other"
-                      type="checkbox"
-                    />
-                  <label className={`${styles.mx_3} ${styles.label}`}>
-                    Other
-                  </label>
-                </div>
-                </div>
-              </div>
               </div>
             </div>
             <div className={styles.formGroup}>

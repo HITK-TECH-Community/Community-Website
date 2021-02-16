32dc0552bc83d30c22473c018186bbc789c9c62a
kajol <kajolkumarisingh222@gmail.com>
2021-02-16 13:02:31 +0530

    🍁 Minor Fix

--- frontend/src/components/auth/login/login.js
+++ frontend/src/components/auth/login/login.js
@@ -71,9 +71,6 @@ const reducer = (state: State, action: Action): State => {
 
 const Login = () => {
   //state for showing and hiding password
-  // const [showPass, setShowPass] = useState(false);
-
-  // const classes = useStyles();
   const [state, dispatch] = useReducer(reducer, initialState);
   const [hidePassword, setHidePassword] = useState(false);
   const passwordInput = useRef("password");

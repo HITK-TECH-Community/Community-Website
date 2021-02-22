0359b7ac8d955f2f437ac1dcf46c439f0d42dc5a
Sakshi Khachane <53635792+sakshikhachane@users.noreply.github.com>
2021-02-22 10:58:23 +0530

    🎧 Enhancement In Add Broadcast Page (#420)
    * Modified add broadcast page
    
    * Done the changes
    
    * Minor change

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -11,7 +11,7 @@ import AllBroadcasts from "./components/broadcast/all_broadcasts/all_broadcasts"
 import Resources from "./components/resource/resources";
 import ContactUs from "./components/contact/contact_us";
 import Admin from "./components/admin/admin";
-import AddBroadcasts from "./components/admin/add_broadcasts/add_broadcasts";
+import AddBroadcasts from "./components/admin/broadcast/add_broadcasts/add_broadcasts";
 import Faqs from "./components/faq/faq";
 import NotFound from "./components/404/not_found";
 import Login from "./components/auth/login/login";

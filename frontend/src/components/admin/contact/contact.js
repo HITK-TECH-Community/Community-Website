799a17a4df3540d6afbd2e69caab39702a0feff1
Aman Prateek <42955585+amanprateek123@users.noreply.github.com>
2021-02-23 11:22:39 +0530

    ♻️ Refactored Admin (#434)
    * Revert "error resoled regarding package.json"
    
    This reverts commit a63ea2586dc7e446743fcb550e13133e6c258a62.
    
    * Revert "Revert "error resoled regarding package.json""
    
    This reverts commit 8946434ff291c760c25617201837b2f688712be1.
    
    * conflict solved
    
    * scrolling behaviour added
    
    * issue
    
    * conflict solved
    
    * scrolling behaviour added
    
    * issue
    
    * conflict solved
    
    * scrolling behaviour added
    
    * issue
    
    * conflict solved
    
    * scrolling behaviour added
    
    * issue
    
    * Admin refactoring
    
    * Code redunduncy of ADD_faq and Add_broadcast
    
    * Revert "Code redunduncy of ADD_faq and Add_broadcast"
    
    This reverts commit 315ede49652f548bfbab37a65830394c7bd9e2e6.

--- frontend/src/components/admin/contact/contact.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Contact() {
-  return (
-    <div>
-      <h1 style={{ textAlign: "center" }}> Contact Us </h1>
-    </div>
-  );
-}
---
+++ frontend/src/pages/Admin/Components/Contact/Contact.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Contact() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}> Contact Us </h1>
+    </div>
+  );
+}

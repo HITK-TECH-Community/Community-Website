7b16873fef7a07eebd3ec5a7e65c37c0de1503f7
amanpra123 <amanpra333@gmail.com>
2021-02-27 23:59:38 +0530

    Admin list is dynamic

--- frontend/src/pages/Admin/Components/Setting/Manage/Manage.jsx
+++ frontend/src/pages/Admin/Components/Setting/Manage/Manage.jsx
@@ -1,16 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import style from "./Manage.module.scss";
 import DeleteIcon from "@material-ui/icons/Delete";
+import { useSelector } from "react-redux";
 
 export function Manage() {
-  const [data, setData] = useState([
-    "Abhishek",
-    "Aman",
-    "Dikshit",
-    "Adarsh",
-    "Utkarsh",
-    "Vatan",
-  ]);
+  const token = useSelector((state) => state.token);
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3500/admin", {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    }).then((res) =>
+      res.json().then((res) => {
+        setData(res);
+      })
+    );
+  }, [token]);
+
+  console.log(data);
+
   const handler = (i) => {
     let a = [...data];
     a.splice(i, 1);
@@ -21,24 +32,28 @@ export function Manage() {
     <div>
       <h1 className={style["h1"]}>Manage Admins</h1>
       <div className={`${style["container"]} container`}>
-        {data.map((item, i) => (
-          <div className={`${style["card"]} card`} key={item + i}>
-            <img
-              src="https://cdn.mos.cms.futurecdn.net/3kZ3hc2YMB6LXiPohtyfKa.jpg"
-              alt="pic"
-              className={style["img"]}
-            />
-            <h3 className={style["h3"]}>{item}</h3>
-            <div>
-              <i className="fab fa-linkedin fa-2x card_footer in"></i>
-              <i className="fab fa-twitter-square fa-2x card_footer"></i>
-              <i className="fab fa-github-square fa-2x card_footer"></i>
-            </div>
-            <div className={style["icon"]} onClick={() => handler(i)}>
-              <DeleteIcon />
-            </div>
-          </div>
-        ))}
+        {data.length > 0
+          ? data.map((item, i) => (
+              <div className={`${style["card"]} card`} key={item + i}>
+                <img
+                  src="https://cdn.mos.cms.futurecdn.net/3kZ3hc2YMB6LXiPohtyfKa.jpg"
+                  alt="pic"
+                  className={style["img"]}
+                />
+                <h3 className={style["h3"]}>
+                  {item.firstName} {item.lastName}
+                </h3>
+                <div className={style["socio"]}>
+                  <i className="fab fa-linkedin fa-2x card_footer in"></i>
+                  <i className="fab fa-twitter-square fa-2x card_footer"></i>
+                  <i className="fab fa-github-square fa-2x card_footer"></i>
+                </div>
+                <div className={style["icon"]} onClick={() => handler(i)}>
+                  <DeleteIcon />
+                </div>
+              </div>
+            ))
+          : null}
       </div>
     </div>
   );

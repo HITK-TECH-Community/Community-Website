02da5eeba122795e714f05d8bd92519b7e40cb05
amanpra123 <amanpra333@gmail.com>
2021-02-28 15:13:30 +0530

    Logged in update

--- frontend/src/components/ErrorHandlerPages/LoggedIn/LoggedIn.jsx
+++ frontend/src/components/ErrorHandlerPages/LoggedIn/LoggedIn.jsx
@@ -5,21 +5,23 @@ import { Button3 } from "../../util/Button/Button";
 export const LoggedIn = () => {
   return (
     <div className={style["not-found"]}>
-      <div className={style["error"]}>
-        <div>
+      <div className={style["error1"]}>
+        <div className={style["img_1"]}>
           <img
-            className={style["error-logo"]}
-            src="./images/error.png"
+            className={style["error-logo1"]}
+            src="http://localhost:3001/images/admin.png"
             alt="404"
           />
         </div>
-        <p className={style["error-text"]}>You are already Logged In!</p>
-        <div className={style["error-content"]}>
-          Please visit here to access your Dashboard.
+        <div className={style["img_2"]}>
+          <p className={style["error-text"]}>You are already Logged In!</p>
+          <div className={style["error-content"]}>
+            Please visit here to access your Dashboard.
+          </div>
+          <a href="/dashboard">
+            <Button3 label="Go to Dashboard!" type="submit" />
+          </a>
         </div>
-        <a href="/dashboard">
-          <Button3 label="Go to Dashboard!" type="submit" />
-        </a>
       </div>
     </div>
   );

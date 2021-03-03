ea253b9d9aabfe4de4cbda9936b3a9d478058973
kajol <kajolkumarisingh222@gmail.com>
2021-03-03 12:32:50 +0530

    ♻️ Invite Admin UI Fix

--- frontend/src/pages/Admin/Components/Setting/Invite/Invite.jsx
+++ frontend/src/pages/Admin/Components/Setting/Invite/Invite.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import { Button2 } from "../../../../../components/util/Button";
 import style from "./Invite.module.scss";
 
 export const Invite = () => {
   return (
     <div className={style["cont"]}>
       <div className="container">
-        <h1 className={style["h1"]}>Invite Admins</h1>
         <div className={style["dash"]}></div>
       </div>
       <div className={style["admin_section"]}>
         <div className={style["admin_card"]}>
+          <h1 className={style["h1"]}>Invite Admins</h1>
           <div className={style["inside_admin"]}>
             <div className={style["form_row"]}></div>
             <div className={style["admin_input"]}>
@@ -22,9 +23,13 @@ export const Invite = () => {
               />
               <i className="far fa-envelope"></i>
             </div>
-          </div>
-          <div className={style["wrap"]}>
-            <button className={style["button"]}>Submit</button>
+            <div className={style["submit-btn"]}>
+              <Button2
+                className={style["submit-btn-text"]}
+                label="Submit"
+                type="submit"
+              />
+            </div>
           </div>
         </div>
       </div>

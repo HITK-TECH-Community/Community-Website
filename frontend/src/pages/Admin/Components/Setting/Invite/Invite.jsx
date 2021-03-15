a3b40cb4898e279a17ba074a233c467279473549
Bishakh Neogi <50258639+bishakhne0gi@users.noreply.github.com>
2021-03-15 10:45:27 +0530

    Added Invite Admin

--- frontend/src/pages/Admin/Components/Setting/Invite/Invite.jsx
+++ frontend/src/pages/Admin/Components/Setting/Invite/Invite.jsx
@@ -1,8 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button2 } from "../../../../../components/util/Button";
 import style from "./Invite.module.scss";
-
 export const Invite = () => {
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState({});
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const isValid = formValidation();
+    if (this.formValidation()) {
+      console.log("Form submitted");
+    } else {
+      console.log("Form has errors.");
+    }
+  };
+
+  const formValidation = () => {
+    const emailError = {};
+    let isValid = true;
+    if (email.trim().length < 1) {
+      isValid = false;
+      emailError.emptyEmail = "* Email cannot be empty";
+    }
+
+    if (typeof email !== "undefined") {
+      let lastAtPos = email.lastIndexOf("@");
+      let lastDotPos = email.lastIndexOf(".");
+
+      if (
+        !(
+          lastAtPos < lastDotPos &&
+          lastAtPos > 0 &&
+          email.indexOf("@@") === -1 &&
+          lastDotPos > 2 &&
+          email.length - lastDotPos > 2
+        )
+      ) {
+        isValid = false;
+        emailError.invalidEmail = "* Email is not valid";
+      }
+    }
+    setEmailError(emailError);
+    return isValid;
+  };
+
   return (
     <div className={style["cont"]}>
       <div className="container">
@@ -19,10 +59,17 @@ export const Invite = () => {
                 id="txt_email"
                 type="text"
                 required="required"
-                name="email"
+                name="Email"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
               />
               <i className="far fa-envelope"></i>
             </div>
+            {Object.keys(emailError).map((key) => {
+              return <div style={{ color: "red" }}>{emailError[key]}</div>;
+            })}
             <div className={style["submit-btn"]}>
               <Button2
                 className={style["submit-btn-text"]}

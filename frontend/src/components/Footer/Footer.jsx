ba90432ffe62b98563a38ec3ef6af20243b01a8e
ShubhamSj07 <shubhamsj077@gmail.com>
2021-03-15 22:52:48 +0530

    Issue Resolved

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,257 +1,259 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import style from "./footer.module.scss";
 
-export class Footer extends React.Component {
-  constructor(props) {
-    super(props);
+export const Footer = () => {
+  //email state
+  const [email, setEmail] = useState("");
 
-    this.state = {
-      fields: {},
-      errors: {},
-    };
-  }
-  handleValidation() {
-    let fields = this.state.fields;
-    let errors = {};
-    let formIsValid = true;
+  //setting email error
+  const [emailErr, setEmailErr] = useState({});
 
-    if (!fields["email"]) {
-      formIsValid = false;
-      errors["email"] = "* Email cannot be empty";
+  //setting up validations
+  const validation = () => {
+    const emailErr = {};
+
+    let isValid = true;
+
+    //email
+    // eslint-disable-next-line
+    const emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (!emailRegex.test(email)) {
+      emailErr.emailRequired = "* Enter Valid Email";
+      isValid = false;
+    }
+    if (!email.trim()) {
+      emailErr.emailRequired = "* Email cannot be empty";
+      isValid = false;
     }
 
-    if (typeof fields["email"] !== "undefined") {
-      let lastAtPos = fields["email"].lastIndexOf("@");
-      let lastDotPos = fields["email"].lastIndexOf(".");
+    //setting up error in state
+    setEmailErr(emailErr);
 
-      if (
-        !(
-          lastAtPos < lastDotPos &&
-          lastAtPos > 0 &&
-          fields["email"].indexOf("@@") === -1 &&
-          lastDotPos > 2 &&
-          fields["email"].length - lastDotPos > 2
-        )
-      ) {
-        formIsValid = false;
-        errors["email"] = "* Email is not valid";
-      }
-    }
-    this.setState({ errors: errors });
-    return formIsValid;
-  }
+    return isValid;
+  };
 
-  contactSubmit(e) {
+  //handling submit
+  const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (this.handleValidation()) {
-      console.log("Form submitted");
-    } else {
-      console.log("Form has errors.");
+    //if isValid = true, form submission trigger
+    const isValid = validation();
+
+    if (isValid) {
+      console.log("Succesfully subcribed for our newsletter!");
+
+      //resetting email value in state after submission of form
+      setEmail("");
     }
-  }
+  };
 
-  handleChange(field, e) {
-    let fields = this.state.fields;
-    fields[field] = e.target.value;
-    this.setState({ fields });
-  }
-  render() {
-    return (
-      <div>
-        <footer className={style["footer"]}>
-          <div className={style["footer-addr"]}>
-            <h1 className={style["footer-logo"]}>HITK Tech Community</h1>
-            <address>
-              <i className="fas fa-map-marker-alt"></i> Heritage Institute of
-              Technology, Chowbaga Road, Kolkata, West Bengal - 700107, India
+  return (
+    <React.Fragment>
+      <footer className={style["footer"]}>
+        <div className={style["footer-addr"]}>
+          <h1 className={style["footer-logo"]}>HITK Tech Community</h1>
+          <address>
+            <i className="fas fa-map-marker-alt"></i> Heritage Institute of
+            Technology, Chowbaga Road, Kolkata, West Bengal - 700107, India
+            <br />
+          </address>
+          <div className={style["newsletter"]}>
+            <h2 className={style["nav-title"]}>Sign Up for our Newsletter</h2>
+            <p>
+              Receive updates and news about various Job Opportunities,
+              Internships, Webinars and Open Source Events.
+            </p>
+
+            <form
+              className="d-flex flex-column flex-md-row align-items-center mt-4"
+              onSubmit={handleSubmit}
+            >
+              <input
+                type="text"
+                className={`${style["input-field-footer"]} py-2`}
+                placeholder="Email Id"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+              />
               <br />
-            </address>
-            <div className={style["newsletter"]}>
-              <h2 className={style["nav-title"]}>Sign Up for our Newsletter</h2>
-              <p>
-                Receive updates and news about various Job Opportunities,
-                Internships, Webinars and Open Source Events.
-              </p>
-
-              <form
-                className="d-flex flex-column flex-md-row align-items-center mt-4"
-                onSubmit={this.contactSubmit.bind(this)}
+              {Object.keys(emailErr).map((key) => {
+                return (
+                  <div
+                    className={`${style["validation"]} d-sm-block d-md-none`}
+                    key={key}
+                  >
+                    {emailErr[key]}
+                  </div>
+                );
+              })}
+              <button
+                type="submit"
+                className={`mt-3 mt-md-0 ${style["submit-btn-footer"]} py-2 px-3 `}
               >
-                <input
-                  type="text"
-                  className={`${style["input-field-footer"]} py-2`}
-                  placeholder="Email Id"
-                  onChange={this.handleChange.bind(this, "email")}
-                  value={this.state.fields["email"]}
-                />
-                <br />
-                <div className={`${style["validation"]} d-sm-block d-md-none`}>
-                  <div>{this.state.errors["email"]}</div>
-                </div>
-                <button
-                  type="submit"
-                  className={`mt-3 mt-md-0 ${style["submit-btn-footer"]} py-2 px-3 `}
+                Sign Up
+              </button>
+            </form>
+            {Object.keys(emailErr).map((key) => {
+              return (
+                <div
+                  className={`${style["validation-new"]} validation-new d-sm-none d-md-block`}
+                  key={key}
                 >
-                  Sign Up
-                </button>
-              </form>
-              <div
-                className={`${style["validation-new"]} validation-new d-sm-none d-md-block`}
-              >
-                <div>{this.state.errors["email"]}</div>
-              </div>
-            </div>
+                  {emailErr[key]}
+                </div>
+              );
+            })}
           </div>
-          <ul className={style["footer-nav"]}>
-            <li className={style["nav-item"]}>
-              <h2 className={style["nav-title"]}>Navigation</h2>
-
-              <ul className={style["nav-ul"]}>
-                <li>
-                  <Link to="/about-us">About</Link>
-                </li>
-
-                <li>
-                  <Link to="/broadcasts">Broadcasts</Link>
-                </li>
-
-                <li>
-                  <Link to="/resources">Resources</Link>
-                </li>
-
-                <li>
-                  <Link to="/contact-us">Contact</Link>
-                </li>
-
-                <li>
-                  <Link to="/faqs">FAQs</Link>
-                </li>
-              </ul>
-            </li>
-
-            <li className={style["nav-item"]}>
-              <h2 className={style["nav-title"]}>Other</h2>
-
-              <ul className={style["nav-ul"]}>
-                <li>
-                  <Link to="/privacy-policy">Privacy Policy</Link>
-                </li>
-
-                <li>
-                  <Link to="/terms">Terms of Use</Link>
-                </li>
-
-                <li>
-                  <Link to="/get-involved">Get Involved</Link>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <h2 className={style["nav-title"]}>Social</h2>
-              <ul className={style["nav-ul"]}>
-                <li>Be sure to give us a follow on the below social links</li>
-              </ul>
-              <div className={`col ${style["col"]}`}>
-                <ul className={style["social"]}>
-                  <li>
-                    <a
-                      href="https://www.linkedin.com/company/hitk-tech-community"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <div className={style["outer"]}>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <div className={style["inner"]}>
-                          <i
-                            className={`fab fa-linkedin fa-lg ${style["fa-linkedin-own"]}`}
-                          ></i>
-                        </div>
-                      </div>
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="https://join.slack.com/t/hitkteckcommunity/shared_invite/zt-jgr1sd87-lhiXHO_x63Kt7h8VBxDaFw"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <div className={style["outer"]}>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <div className={style["inner"]}>
-                          <i
-                            className={`fab fa-slack fa-lg ${style["fa-slack-own"]}`}
-                          ></i>
-                        </div>
-                      </div>
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="mailto:hitktechcommunity@gmail.com"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <div className={style["outer"]}>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <div className={style["inner"]}>
-                          <i
-                            className={`fas fa-envelope fa-lg ${style["fa-envelope-own"]}`}
-                          ></i>
-                        </div>
-                      </div>
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="https://github.com/HITK-TECH-Community/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <div className={style["outer"]}>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <div className={style["inner"]}>
-                          <i
-                            className={`fab fa-github fa-lg ${style["fa-github-own"]}`}
-                          ></i>
-                        </div>
-                      </div>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </li>
-          </ul>
-          <div className={style["footer-dash"]}>
-            <div className={style["footer-text"]}>
-              <p>
-                Made with{" "}
-                <i className="fas fa-heart" style={{ color: "#DB3328" }}></i> by
-                HITK Tech Community
-              </p>
-            </div>
-          </div>
-        </footer>
-
-        <div className={style["cprt-text"]}>
-          <p className={`${style["cprt"]} py-2`}>
-            Copyright © 2021 HITK Tech Community
-          </p>
         </div>
+        <ul className={style["footer-nav"]}>
+          <li className={style["nav-item"]}>
+            <h2 className={style["nav-title"]}>Navigation</h2>
+
+            <ul className={style["nav-ul"]}>
+              <li>
+                <Link to="/about-us">About</Link>
+              </li>
+
+              <li>
+                <Link to="/broadcasts">Broadcasts</Link>
+              </li>
+
+              <li>
+                <Link to="/resources">Resources</Link>
+              </li>
+
+              <li>
+                <Link to="/contact-us">Contact</Link>
+              </li>
+
+              <li>
+                <Link to="/faqs">FAQs</Link>
+              </li>
+            </ul>
+          </li>
+
+          <li className={style["nav-item"]}>
+            <h2 className={style["nav-title"]}>Other</h2>
+
+            <ul className={style["nav-ul"]}>
+              <li>
+                <Link to="/privacy-policy">Privacy Policy</Link>
+              </li>
+
+              <li>
+                <Link to="/terms">Terms of Use</Link>
+              </li>
+
+              <li>
+                <Link to="/get-involved">Get Involved</Link>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <h2 className={style["nav-title"]}>Social</h2>
+            <ul className={style["nav-ul"]}>
+              <li>Be sure to give us a follow on the below social links</li>
+            </ul>
+            <div className={`col ${style["col"]}`}>
+              <ul className={style["social"]}>
+                <li>
+                  <a
+                    href="https://www.linkedin.com/company/hitk-tech-community"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <div className={style["outer"]}>
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                      <div className={style["inner"]}>
+                        <i
+                          className={`fab fa-linkedin fa-lg ${style["fa-linkedin-own"]}`}
+                        ></i>
+                      </div>
+                    </div>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://join.slack.com/t/hitkteckcommunity/shared_invite/zt-jgr1sd87-lhiXHO_x63Kt7h8VBxDaFw"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <div className={style["outer"]}>
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                      <div className={style["inner"]}>
+                        <i
+                          className={`fab fa-slack fa-lg ${style["fa-slack-own"]}`}
+                        ></i>
+                      </div>
+                    </div>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="mailto:hitktechcommunity@gmail.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <div className={style["outer"]}>
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                      <div className={style["inner"]}>
+                        <i
+                          className={`fas fa-envelope fa-lg ${style["fa-envelope-own"]}`}
+                        ></i>
+                      </div>
+                    </div>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://github.com/HITK-TECH-Community/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <div className={style["outer"]}>
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                      <div className={style["inner"]}>
+                        <i
+                          className={`fab fa-github fa-lg ${style["fa-github-own"]}`}
+                        ></i>
+                      </div>
+                    </div>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+        <div className={style["footer-dash"]}>
+          <div className={style["footer-text"]}>
+            <p>
+              Made with
+              <i className="fas fa-heart" style={{ color: "#DB3328" }}></i>
+              by HITK Tech Community
+            </p>
+          </div>
+        </div>
+      </footer>
+
+      <div className={style["cprt-text"]}>
+        <p className={`${style["cprt"]} py-2`}>
+          Copyright © 2020 HITK Tech Community
+        </p>
       </div>
-    );
-  }
-}
+    </React.Fragment>
+  );
+};
--- frontend/src/pages/ContactUs/ContactUs.jsx
+++ frontend/src/pages/ContactUs/ContactUs.jsx
@@ -1,8 +1,107 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button2 } from "../../components/util/Button/index";
 import style from "./ContactUs.module.scss";
 
 export const ContactUs = () => {
+  //setting form values
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [msg, setMsg] = useState("");
+
+  //setting custom form error messages
+  const [nameErr, setNameErr] = useState({});
+  const [emailErr, setEmailErr] = useState({});
+  const [subjectErr, setSubjectErr] = useState({});
+  const [msgErr, setMsgErr] = useState({});
+
+  const [submited, setSubmited] = useState(false);
+
+  //setting form validations
+  const validation = () => {
+    const nameErr = {};
+    const emailErr = {};
+    const subjectErr = {};
+    const msgErr = {};
+
+    let isValid = true;
+
+    //name
+    if (!name.trim()) {
+      nameErr.nameRequired = "* Name is required ";
+      isValid = false;
+    } else if (name.trim().length < 3) {
+      nameErr.nameRequired = "* Name must be atleast 3 characters";
+      isValid = false;
+    }
+
+    if (name.match("^\\d+$")) {
+      nameErr.nameRequired = "* Name must be in characters";
+      isValid = false;
+    }
+
+    //email
+    // eslint-disable-next-line
+    const emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (!emailRegex.test(email)) {
+      emailErr.emailRequired = "* Please Enter Valid Email";
+      isValid = false;
+    }
+    if (!email.trim()) {
+      emailErr.emailRequired = "* Email is required";
+      isValid = false;
+    }
+
+    //subject
+    if (subject.trim().length < 5) {
+      subjectErr.subjectRequired = "* Subject must be atleast 5 characters";
+      isValid = false;
+    }
+    if (!subject.trim()) {
+      subjectErr.subjectRequired = "* Subject is required";
+
+      isValid = false;
+    }
+
+    //message
+    if (msg.trim().length < 8) {
+      msgErr.msgRequired = "* Message must be atleast 8 characters";
+
+      isValid = false;
+    }
+    if (!msg.trim()) {
+      msgErr.msgRequired = "* Message is required";
+
+      isValid = false;
+    }
+
+    //setting up errors in state
+    setNameErr(nameErr);
+    setEmailErr(emailErr);
+    setSubjectErr(subjectErr);
+    setMsgErr(msgErr);
+
+    return isValid;
+  };
+
+  //handling submit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //if isValid = true, form submission trigger
+    const isValid = validation();
+
+    if (isValid) {
+      setSubmited(true);
+
+      //resetting values in state after submission of form
+      setName("");
+      setEmail("");
+      setSubject("");
+      setMsg("");
+    }
+  };
   return (
     <div className={style["contact-section"]}>
       <div className={style["contact-parent"]}>
@@ -13,60 +112,111 @@ export const ContactUs = () => {
             className={style["contact-image"]}
           />
         </div>
-
         <div className={`${style["contact-child"]} ${style["child2"]}`}>
           <div className={style["contact-card"]}>
-            <h1 className={style["contact-header-text"]}>Get in touch</h1>
-            <div className={style["inside-contact"]}>
-              <div className={style["contact-input"]}>
-                <input
-                  id="txt_name"
-                  type="text"
-                  required="required"
-                  name="name"
-                  placeholder="Your Name"
-                />
-                <i className="fas fa-user"></i>
-              </div>
-              <div className={style["contact-input"]}>
-                <input
-                  id="txt_email"
-                  type="text"
-                  required="required"
-                  name="email"
-                  placeholder="Your Email"
-                />
-                <i className="fas fa-envelope-open-text"></i>
-              </div>
-              <div className={style["contact-input"]}>
-                <input
-                  id="txt_subject"
-                  type="text"
-                  required="required"
-                  name="subject"
-                  placeholder="Your Subject"
-                />
-                <i className="fas fa-pencil-alt"></i>
-              </div>
-              <div className={style["contact-input"]}>
-                <textarea
-                  id="txt_message"
-                  rows="4"
-                  cols="20"
-                  name="message"
-                  required="required"
-                  placeholder="Your Message"
-                ></textarea>
-                <i className="fas fa-comment-dots"></i>
-              </div>
-              <div className={style["submit-btn"]}>
-                <Button2
-                  className={style["submit-btn-text"]}
-                  label="Let's Talk!"
-                  type="submit"
-                />
-              </div>
-            </div>
+            {submited ? (
+              <React.Fragment>
+                <div className={style["inside-card"]}>
+                  <h1 className={style["card-heading"]}>
+                    We have heard you!! <br /> We will soon get back to you if
+                    required!
+                  </h1>
+                </div>
+              </React.Fragment>
+            ) : (
+              <React.Fragment>
+                <h1 className={style["contact-header-text"]}>Get In Touch</h1>
+                <div className={style["inside-contact"]}>
+                  <form onSubmit={handleSubmit}>
+                    <div className={style["contact-input"]}>
+                      <input
+                        autoFocus="on"
+                        autoComplete="off"
+                        name="name"
+                        id="txt_name"
+                        type="text"
+                        placeholder="Your Name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                      />
+                      <i className="fas fa-user"></i>
+                      {Object.keys(nameErr).map((key) => {
+                        return (
+                          <div className={style["validation"]} key={key}>
+                            {nameErr[key]}
+                          </div>
+                        );
+                      })}
+                    </div>
+                    <div className={style["contact-input"]}>
+                      <input
+                        autoComplete="off"
+                        name="email"
+                        id="txt_email"
+                        type="text"
+                        placeholder="Your Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                      <i className="fas fa-envelope-open-text"></i>
+                      {Object.keys(emailErr).map((key) => {
+                        return (
+                          <div className={style["validation"]} key={key}>
+                            {emailErr[key]}
+                          </div>
+                        );
+                      })}
+                    </div>
+                    <div className={style["contact-input"]}>
+                      <input
+                        autoComplete="off"
+                        name="subject"
+                        id="txt_subject"
+                        type="text"
+                        placeholder="Your Subject"
+                        value={subject}
+                        onChange={(e) => setSubject(e.target.value)}
+                      />
+                      <i className="fas fa-pencil-alt"></i>
+                      {Object.keys(subjectErr).map((key) => {
+                        return (
+                          <div className={style["validation"]} key={key}>
+                            {subjectErr[key]}
+                          </div>
+                        );
+                      })}
+                    </div>
+                    <div className={style["contact-input"]}>
+                      <textarea
+                        autoComplete="off"
+                        name="message"
+                        id="txt_message"
+                        rows="4"
+                        cols="20"
+                        placeholder="Your Message"
+                        value={msg}
+                        onChange={(e) => setMsg(e.target.value)}
+                      ></textarea>
+                      <i className="fas fa-comment-dots"></i>
+                      {Object.keys(msgErr).map((key) => {
+                        return (
+                          <div className={style["validation"]} key={key}>
+                            {msgErr[key]}
+                          </div>
+                        );
+                      })}
+                    </div>
+                    <div className={style["submit-btn"]}>
+                      <Button2
+                        className={style["submit-btn-text"]}
+                        label="Let's Talk!"
+                        type="submit"
+                      />
+                    </div>
+                  </form>
+                </div>
+              </React.Fragment>
+            )}
           </div>
         </div>
       </div>

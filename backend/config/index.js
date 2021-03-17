0aa042aaccedbf7ee4985dbff8be9a0fabf777ae
AmeyaJain-25 <ameiya.jain@hotmail.com>
2021-03-17 22:16:22 +0530

    Rewuired changes made

--- backend/config/index.js
+++ backend/config/index.js
@@ -2,10 +2,10 @@
 require('dotenv').config();
 
 const config = {
-  "MONGO_DB_URL": process.env.MONGODBURL,
-  "JWT_SECRET_KEY": process.env.JWTSECRETKEY,
-  "JWT_EXPIRES_IN" : process.env.JWTEXPIRESIN,
-  "baseURL" : process.env.BASEURL,
+  "MONGO_DB_URL": process.env.MONGO_DB_URL,
+  "JWT_SECRET_KEY": process.env.JWT_SECRET_KEY,
+  "JWT_EXPIRES_IN" : process.env.JWT_EXPIRES_IN,
+  "BASE_URL" : process.env.BASE_URL,
 }
 
 module.exports = config;

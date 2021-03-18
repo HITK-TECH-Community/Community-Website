1edfbc40e16fc582df16a8ffd3c837602bcab1e9
Coder_Srinivas <srinivas.babu364@gmail.com>
2021-03-18 18:49:11 +0530

    🌞 Added Dark Theme (#542)

--- frontend/src/components/util/Toggle/Toggle.jsx
+++ frontend/src/components/util/Toggle/Toggle.jsx
@@ -4,9 +4,17 @@ import style from "./toggle.module.scss";
 export const Toggle = (props) => {
   return (
     <div className={style["container"]}>
-      <label className={style["switch"]}>
-        <input type="checkbox" onChange={() => props.handleClick()} />
-        <span className={`${style["slider"]} ${style["round"]}`}></span>
+      <input
+        type="checkbox"
+        id="check"
+        className={style["checkbox"]}
+        checked={props.theme}
+        onChange={() => props.handleClick()}
+      />
+      <label htmlFor="check" className={style["label"]}>
+        <i className={`fas fa-moon ${style["moon"]}`}></i>
+        <i className={`fas fa-sun ${style["sun"]}`}></i>
+        <div className={style["slider"]}></div>
       </label>
     </div>
   );

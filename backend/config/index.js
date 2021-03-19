e95033f74ee565a8de7fdd09709c003b0fbc9ce7
Rohit Agarwal <agaroh@amazon.com>
2021-03-19 18:53:05 +0530

    fixes config object

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 
 const config = {
-  "MONGO_DB_URL": process.env.MONGO_DB_URL,
-  "JWT_SECRET_KEY": process.env.JWT_SECRET_KEY,
-  "JWT_EXPIRES_IN" : process.env.JWT_EXPIRES_IN,
-  "BASE_URL" : process.env.BASE_URL,
-}
+  MONGO_DB_URL: process.env.MONGO_DB_URL,
+  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
+  BASE_URL: process.env.BASE_URL,
+};
 
 module.exports = config;

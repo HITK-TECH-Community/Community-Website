43f7eab6a2fe8b2761b87ce0c00cb97d19cd2780
Akshita Kanojia <44370673+akshitakanojia@users.noreply.github.com>
2021-03-20 17:35:25 +0530

    🌻Login Form Validation Added (#545)
    * Login form validaion added(#537)
    
    * Login validation changes implemented

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -6,6 +6,7 @@ import * as actions from "../../store/actions/actions";
 import { END_POINT } from "../../config/api";
 import { SimpleToast } from "../../components/util/Toast";
 import { Link } from "react-router-dom";
+import Joi from "joi-browser";
 
 export function Login() {
   const [hidePassword, setHidePassword] = useState(false);
@@ -14,11 +15,47 @@ export function Login() {
   const [credential, setCredential] = useState(schema);
   const dispatch = useDispatch();
 
-  const handleChange = e => {
-    setCredential({ ...credential, [e.target.name]: e.target.value });
+  const [errorObj, setErrorObj] = useState({});
+
+  const validationSchema = {
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
   };
 
-  const logout = useSelector(state => state.logout);
+  const isFormValid = () => {
+    const check = Joi.validate(credential, validationSchema, {
+      abortEarly: false,
+    });
+    if (!check.error) return true;
+    const errors = {};
+    check.error.details.map((item) => {
+      if (!errors[item.path[0]]) errors[item.path[0]] = item.message;
+    });
+    setErrorObj(errors);
+    return false;
+  };
+
+  const validateField = (input) => {
+    const { name, value } = input;
+    const obj = { [name]: value };
+    const obj_schema = { [name]: validationSchema[name] };
+    const result = Joi.validate(obj, obj_schema);
+
+    return result.error ? result.error.details[0].message : null;
+  };
+
+  const handleChange = (e) => {
+    const { currentTarget: input } = e;
+    const errors = { ...errorObj };
+    const errorMessage = validateField(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
+    setCredential({ ...credential, [e.target.name]: e.target.value });
+    setErrorObj(errors);
+  };
+
+  const logout = useSelector((state) => state.logout);
   const [openLogoutSuccess, setOpenLogoutSuccessToast] = React.useState(false);
   const [openError1, setOpenError1Toast] = React.useState(false); //backend error
   const [openError2, setOpenError2Toast] = React.useState(false); //unauthorized
@@ -44,36 +81,40 @@ export function Login() {
 
   function loginUser(e) {
     e.preventDefault();
-    return fetch(`${END_POINT}/auth/login`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(credential),
-  })
-      .then(response =>
-        response
-          .json()
-          .then(res => {
-            if (response.status === 200) {
-              localStorage.setItem("token", res.token);
-              localStorage.setItem("isSuperAdmin", res.isSuperAdmin);
-              window.location = "/dashboard?loggedin";
-            } else if (response.status === 400) {
-              setOpenError2Toast(true);
-            } else {
-              setOpenError3Toast(true);
-            }
-          })
-          .catch(err => setOpenError3Toast(true))
-      )
-      .catch(err => {
-        setOpenError1Toast(true);
-        console.error("must be a backend problem🤔:", err);
-      });
+    if (isFormValid()) {
+      return fetch(`${END_POINT}/auth/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(credential),
+      })
+        .then((response) =>
+          response
+            .json()
+            .then((res) => {
+              if (response.status === 200) {
+                localStorage.setItem("token", res.token);
+                localStorage.setItem("isSuperAdmin", res.isSuperAdmin);
+                window.location = "/dashboard?loggedin";
+              } else if (response.status === 400) {
+                setOpenError2Toast(true);
+              } else {
+                setOpenError3Toast(true);
+              }
+            })
+            .catch((err) => setOpenError3Toast(true))
+        )
+        .catch((err) => {
+          setOpenError1Toast(true);
+          console.error("must be a backend problem🤔:", err);
+        });
+    }
   }
 
-  hidePassword ? (passwordInput.current = "text") : (passwordInput.current = "password");
+  hidePassword
+    ? (passwordInput.current = "text")
+    : (passwordInput.current = "password");
 
   return (
     <>
@@ -84,25 +125,28 @@ export function Login() {
         <div className={`${style["login-form"]} ${style["child2"]}`}>
           <div className={style["login-card"]}>
             <h1 className={style["card-heading"]}>Welcome Back</h1>
-            <form onSubmit={loginUser}>
+            <form onSubmit={loginUser} noValidate>
               <div className={style["inside-card"]}>
                 <div className={style["login-input"]}>
                   <input
                     autocomplete="off"
                     id="username"
                     type="email"
-                    required="required"
                     name="email"
                     placeholder="Username"
                     onChange={handleChange}
                     className={style["input-login"]}
                   />
                   <i className="fas fa-user"></i>
+                  <div
+                    className={`${style["validation"]} d-sm-none d-md-block`}
+                  >
+                    {errorObj["email"] && <div>* {errorObj["email"]}</div>}
+                  </div>
                 </div>
                 <div className={style["login-input"]}>
                   <input
                     id="password"
-                    required="required"
                     name="password"
                     placeholder="Password"
                     className={style["input-login"]}
@@ -113,9 +157,21 @@ export function Login() {
                     className={hidePassword ? "fa fa-eye" : "fa fa-eye-slash"}
                     onClick={() => setHidePassword(!hidePassword)}
                   ></i>
+                  <div
+                    className={`${style["validation"]} d-sm-none d-md-block`}
+                  >
+                    {errorObj["password"] && (
+                      <div>* {errorObj["password"]}</div>
+                    )}
+                  </div>
                 </div>
                 <div className={style["submit-btn"]}>
-                  <Button2 id="btn" label="Sign In" type="submit" className={style["submit-btn-text"]} />
+                  <Button2
+                    id="btn"
+                    label="Sign In"
+                    type="submit"
+                    className={style["submit-btn-text"]}
+                  />
                 </div>
                 <Link to="/forgot-password">
                   <h5 style={{ textAlign: "center" }}>Forgot your password?</h5>

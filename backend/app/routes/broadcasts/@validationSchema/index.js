fbec16093343625ab33f8974b545772c08f113e7
Aman Jha <aman.jha21.aj@gmail.com>
2021-03-20 13:53:41 +0545

    validation required for uri

--- backend/app/routes/broadcasts/@validationSchema/index.js
+++ backend/app/routes/broadcasts/@validationSchema/index.js
@@ -3,9 +3,9 @@ const Joi = require('joi');
 const addBroadcastValidation = Joi.object({
   title: Joi.string().required(),
   content: Joi.string().required(),
-  link: Joi.string().uri(),
+  link: Joi.string().uri().required(),
   isExpired: Joi.boolean().required(),
-  imageUrl: Joi.string().uri(),
+  imageUrl: Joi.string().uri().required(),
   category: Joi.string().required(),
 });
 

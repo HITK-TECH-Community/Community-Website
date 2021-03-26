b45efaaa54016cbd2fa5b88ef8e2613c06510762
AbhipsaGuru1012 <58511560+AbhipsaGuru1012@users.noreply.github.com>
2021-03-26 14:09:38 +0530

    ♻️ Enhancement In Forms (#540)
    * Enhancement in forms
    
    * Modification in the form
    
    * Delete package-lock.json
    
    * Delete package.json
    
    * Delete LICENSE
    
    * Delete README.md
    
    * Delete react-recaptcha.js
    
    * modification

--- frontend/src/pages/ForgotPassword/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword/ForgotPassword.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Joi from "joi-browser";
 import { Button1, Button2 } from "../../components/util/Button/index";
 import style from "./ForgotPassword.module.scss";
 // import { END_POINT } from "../../config/api";
@@ -10,12 +11,80 @@ export function ForgotPassword() {
   const [status, setStatus] = useState("");
   const [submited, setSubmited] = useState(false);
 
+  const [formdata, setFormData] = useState({
+    email: "",
+    // trust: null,
+  });
+
+  const [formerrors, setFormErrors] = useState({});
+
+  const schema = {
+    email: Joi.string().email().min(3).max(100).required(),
+    // trust: Joi.required(),
+  };
+
+  const validate = () => {
+    const result = Joi.validate(formdata, schema, { abortEarly: false });
+    if (!result.error) return true;
+    const errors = {};
+    for (let item of result.error.details) {
+      errors[item.path[0]] = item.message;
+    }
+    // result.error.details.map((item) => {
+    //   if (!errors[item.path[0]]) errors[item.path[0]] = item.message;
+    // });
+    // setErrorObj(errors);
+    // return false;
+    return errors;
+  };
+
+  const validateProperty = (input) => {
+    const { name, value } = input;
+    const obj = { [name]: value };
+    const obj_schema = { [name]: schema[name] };
+    const result = Joi.validate(obj, obj_schema);
+    return result.error ? result.error.details[0].message : null;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const errors = validate();
+    Object.keys(formdata).map((key) => {
+      if (formdata[key] === "" || formdata[key] === null) {
+        errors[key] = `${key} is not allowed to be empty`;
+      }
+    });
+    if (errors["info"]) {
+      delete errors["info"];
+    }
+    if (Object.keys(errors).length !== 0) {
+      setFormErrors(errors);
+    }
+    if (Object.keys(errors).length !== 0) {
+      console.log(errors);
+    } else {
+      //Call the Server
+      console.log("Submitted");
+      submitForgotPassword();
+    }
+  };
+
   const handleChange = (e) => {
-    setEmail(e.target.value);
+    const { currentTarget: input } = e;
+    const errors = { ...formerrors };
+    const errorMessage = validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
+    const data = { ...formdata };
+    data[input.name] = input.value;
+    setFormData({ ...data, [input.name]: input.value });
+    setFormErrors(errors);
+    // setEmail(e.target.value);
   };
 
   function submitForgotPassword(e) {
-    e.preventDefault();
+    // e.preventDefault();
     return setSubmited(true);
     // TODO : Connect with backend Api
     // return fetch(`${END_POINT}/auth/forgot-password`, {
@@ -79,22 +148,41 @@ export function ForgotPassword() {
             ) : (
               <React.Fragment>
                 <h1 className={style["card-heading"]}>Trouble Logging In?</h1>
-                <form onSubmit={submitForgotPassword}>
+                <form onSubmit={handleSubmit} noValidate>
                   <div className={style["inside-card"]}>
-                    <div className={style["forgot-password-input"]}>
-                      <input
-                        autocomplete="off"
-                        id="username"
-                        type="email"
-                        required="required"
-                        name="email"
-                        placeholder="Email"
-                        onChange={handleChange}
-                        className={style["input-forgot-password"]}
-                        onInvalid={(e)=>{ e.target.setCustomValidity("Enter a valid Email Id") }}
-                        onInput={(e)=>{ e.target.value?.match(new RegExp(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/)) && e.target.setCustomValidity("") }}
-                      />
-                      <i className="fas fa-at"></i>
+                    <div className={`form-group ${style["form-group"]}`}>
+                      <div className={style["forgot-password-input"]}>
+                        <input
+                          autocomplete="off"
+                          id="username"
+                          type="email"
+                          name="email"
+                          placeholder="Email"
+                          onChange={handleChange}
+                          className={style["input-forgot-password"]}
+                          // onInvalid={(e) => {
+                          //   e.target.setCustomValidity(
+                          //     "Enter a valid Email Id"
+                          //   );
+                          // }}
+                          // onInput={(e) => {
+                          //   e.target.value?.match(
+                          //     new RegExp(
+                          //       /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/
+                          //     )
+                          //   ) && e.target.setCustomValidity("");
+                          // }}
+                        />
+                        <i className="fas fa-at"></i>
+                        <div
+                          className={`${style["validation"]} validation d-sm-none d-md-block`}
+                        >
+                          {formerrors["email"] && (
+                            <div>* {formerrors["email"]}</div>
+                          )}
+                          {/* {errorObj["email"] && <div>* {errorObj["email"]}</div>} */}
+                        </div>
+                      </div>
                     </div>
                     <div className={style["submit-btn"]}>
                       <Button2

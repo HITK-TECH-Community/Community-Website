77b690b976c4da4192fd8f6c03bdfa28efb02e00
mrunalvilas <mrunalvilas@gmail.com>
2021-03-30 10:15:55 +0530

    ♻️ Added Validation In Add FAQ Form (#549)
    * adding validation to ADD-FAQ 1.empty field 2.length should be of 8 characters
    
    * validating ADD_FAQ
    
    * modifies single line gab between styles of Add-FAQ file
    
    * making changes to AddFAQ page's validation
    
    1. deleting default validations
    2. arranging validation message right below respective field
    
    * Changing Padding of validation messages.
    
    * changing CSS for adjusting  padding of validation messages in ADD-FAQ component of Admin Dashboard

--- frontend/src/pages/Admin/Components/Faq/AddFaq/AddFaq.jsx
+++ frontend/src/pages/Admin/Components/Faq/AddFaq/AddFaq.jsx
@@ -2,33 +2,115 @@ import React from "react";
 import styles from "./add-faq.module.scss";
 import { Button2 } from "../../../../../components/util/Button/index";
 
-export function AddFaq() {
+export class AddFaq extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fields: {},
+      errors: {},
+    };
+  }
+  handleValidation() {
+    let fields = this.state.fields;
+    let errors = {};
+    let formIsValid = true;
+
+    if (!fields["question"]) {
+      formIsValid = false;
+      errors["question"] = "* Question cannot be empty";
+    }
+
+    if (!fields["answer"]) {
+      formIsValid = false;
+      errors["answer"] = "* Answer cannot be empty";
+    }
+
+    if (typeof fields["question"] !== "undefined") {
+
+      if (
+        !(
+          fields["question"].length > 0 && fields["question"].length < 9
+        )
+      ) {
+        formIsValid = false;
+        errors["question"] = "* Question should consists maximum 8 characters";
+      }
+    }
+
+    if (typeof fields["answer"] !== "undefined") {
+
+      if (
+        !(
+          fields["answer"].length > 0 && fields["answer"].length < 9
+        )
+      ) {
+        formIsValid = false;
+        errors["answer"] = "* Answer should consists maximum 8 characters";
+      }
+    }
+    this.setState({ errors: errors });
+    return formIsValid;
+  }
+
+  contactSubmit(e) {
+    e.preventDefault();
+
+    if (this.handleValidation()) {
+      console.log("Form submitted");
+    } else {
+      console.log("Form has errors.");
+    }
+  }
+
+  handleChange(field, e) {
+    let fields = this.state.fields;
+    fields[field] = e.target.value;
+    this.setState({ fields });
+  }
+
+
+  render () {
+   
   return (
-    <div className={styles["faq-section"]}>
+    <div className={styles["faq-section"]} >
       <div className={styles["faq-parent"]}>
         <div className={styles["faq-child"] + " " + styles["child1"]}>
           <div className={styles["faq-card"]}>
             <h1 className={styles["faq-header-text"]}>Add &nbsp; Faq</h1>
             <div className={styles["inside-faq"]}>
+              <form onSubmit={this.contactSubmit.bind(this)}>
               <div className={styles["faq-input"]}>
                 <input
-                  id="txt_name"
+                  id="question"
                   type="text"
-                  required="required"
                   name="question"
                   placeholder="Question"
+                  onChange={this.handleChange.bind(this, "question")}
+                  value={this.state.fields["question"]}
                 />
                 <i className="fas fa-question-circle"></i>
+                <br />
+                <div>
+                  <div className={`${styles["validation"]} validation`}>{this.state.errors["question"]}</div>
+                </div>
+                <br />
               </div>
+              
               <div className={styles["faq-input"]}>
                 <input
-                  id="txt_email"
+                  id="answer"
                   type="text"
-                  required="required"
                   name="answer"
                   placeholder="Answer"
+                  onChange={this.handleChange.bind(this, "answer")}
+                  value={this.state.fields["answer"]}
                 />
                 <i className="fas fa-comment-dots"></i>
+                <br />
+                <div>
+                  <div className={`${styles["validation"]} validation`}>{this.state.errors["answer"]}</div>
+                </div>
+                <br />
               </div>
               <div className={styles["submit-btn"]}>
                 <Button2
@@ -37,10 +119,13 @@ export function AddFaq() {
                   type="submit"
                 />
               </div>
-            </div>
+              </form>
+            </div> 
+             
           </div>
         </div>
       </div>
     </div>
   );
+  }
 }

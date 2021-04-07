f080d9744b98bd857a11d38b48a104179a58ef14
Dhwaj Gupta <57860123+dhwaj1902@users.noreply.github.com>
2021-04-07 21:33:47 +0530

    ♻️ Loader Added (#597)
    * loader added in manage admin
    
    * Loader Update

--- frontend/src/pages/Admin/Components/Setting/Manage/Manage.jsx
+++ frontend/src/pages/Admin/Components/Setting/Manage/Manage.jsx
@@ -3,22 +3,29 @@ import style from "./Manage.module.scss";
 import DeleteIcon from "@material-ui/icons/Delete";
 import { useSelector } from "react-redux";
 import { END_POINT } from "../../../../../config/api";
-
+import { Spinner } from "reactstrap";
+import Loader from "../../../../../components/util/Loader";
 export function Manage() {
   const token = useSelector((state) => state.token);
 
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch(`${END_POINT}/admin`, {
       headers: {
         Authorization: "Bearer " + token,
       },
-    }).then((res) =>
-      res.json().then((res) => {
-        setData(res);
-      })
-    );
+    })
+      .then((res) =>
+        res.json().then((res) => {
+          setData(res);
+          setLoading(false);
+        })
+      )
+      .catch((error) => {
+        setLoading(false);
+      });
   }, [token]);
 
   const handler = (i) => {
@@ -29,8 +36,9 @@ export function Manage() {
   return (
     <div>
       <h1 className={style["h1"]}>Manage Admins</h1>
+      {loading && <Loader color="primary" size="lg" />}
       <div className={`${style["container"]} container`}>
-        {data.length > 0
+        {data.length > 0 && !loading
           ? data.map((item, i) => (
               <div className={`${style["card"]} card`} key={item + i}>
                 <img
@@ -53,12 +61,12 @@ export function Manage() {
             ))
           : null}
       </div>
-      {data.length === 0 ? 
+      {data.length === 0 && !loading ? (
         <div className={style["no-admin"]}>
-          <img src="./images/admin.png" width="20%" alt=""/>
-              No Admin Found!
-        </div> 
-      : null}
+          <img src="./images/admin.png" width="20%" alt="" />
+          No Admin Found!
+        </div>
+      ) : null}
     </div>
   );
 }

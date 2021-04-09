a9e17acb94d912ebf4adce7d64debaa5834801bc
Rohit Agarwal <agaroh@amazon.com>
2021-04-09 16:32:22 +0530

    Modifies mocha test command and tests folder

--- backend/tests/routes/broadcast/postBroadcast.test.js
+++ backend/tests/routes/broadcast/postBroadcast.test.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const { expect } = require('chai');
 const chaiHttp = require('chai-http');
-const server = require('../index');
+const server = require('../../../index');
 
 chai.use(chaiHttp);
 

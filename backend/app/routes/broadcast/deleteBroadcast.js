a2bfa2298c3ed4aa91a77ccd4f8835f172bb8a2b
Aman Jha <aman.jha21.aj@gmail.com>
2021-04-11 21:38:20 +0545

    error handler added

--- backend/app/routes/broadcast/deleteBroadcast.js
+++ backend/app/routes/broadcast/deleteBroadcast.js
@@ -6,15 +6,16 @@ const constants = require('../../../constants');
 module.exports = async (req, res, next) => {
   const [err, broadcast] = await to(Broadcast.findByIdAndDelete(req.params.id));
   if (!broadcast) {
-    res.status(400).send({
+    const error = new ErrorHandler(constants.ERRORS.DATABASE, {
+      statusCode: 400,
       message: "Broadcast doesn't exist",
     });
-    return next();
+    return next(error);
   }
   if (err) {
     const error = new ErrorHandler(constants.ERRORS.DATABASE, {
       statusCode: 500,
-      message: 'Mongo Error: Insertion Failed',
+      message: 'Mongo Error: Deletion Failed',
       errStack: err,
     });
     return next(error);

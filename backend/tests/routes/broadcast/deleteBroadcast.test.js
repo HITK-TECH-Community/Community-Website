ff1691882127b4cc2b847bd86778d50ccfef23ca
Aman Jha <aman.jha21.aj@gmail.com>
2021-04-11 21:45:07 +0545

    firstly add broadcast then delete

--- backend/tests/routes/broadcast/deleteBroadcast.test.js
+++ backend/tests/routes/broadcast/deleteBroadcast.test.js
@@ -1,13 +1,14 @@
 const chai = require('chai');
 const { expect } = require('chai');
 const chaiHttp = require('chai-http');
-const server = require('../index');
+const server = require('../../../index');
 
 chai.use(chaiHttp);
 
 // Test for delete Broadcast
 describe('Test for delete Broadcast:', () => {
   let token = '';
+  let id = '';
 
   // Step 1 - login as admin
   it('log in as admin at /auth/login', (done) => {
@@ -31,9 +32,34 @@ describe('Test for delete Broadcast:', () => {
       .catch(done);
   });
 
-  // Step 2 - delete broadcast from DB
+  // Step 2 - add broadcast to DB
+  it('add broadcast to DB at /broadcast', (done) => {
+    const broadcastData = {
+      title: 'testtitle',
+      content:
+        '--this is test content ----this is test content ----this is test content ----this is test content ----this is test content ----this is test content ----this is test content ----this is test content ----this is test content ----this is test content ----this is test content ----this is test content ----this is test content ----this is test content --',
+      link: 'https://abc.xyz',
+      expiresOn: new Date(),
+      imageUrl: ['http://imageurl@imageurl.com', 'http://imageurl@imageurl.com', 'http://imageurl@imageurl.com'],
+      tags: ['tag1', 'tag2', 'tag3'],
+    };
+
+    chai
+      .request(server)
+      .post('/broadcast')
+      .set('Authorization', `Bearer ${token}`)
+      .send(broadcastData)
+      .then((res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.message).to.equal('Broadcast added successfully');
+        id = res.body.id;
+        done();
+      })
+      .catch(done);
+  });
+
+  // Step 3 - delete broadcast from DB
   it('delete broadcast from DB at /broadcast/:id', (done) => {
-    const id = '6071b03b2e497b2f4806e90f';
     chai
       .request(server)
       .delete(`/broadcast/${id}`)
@@ -48,7 +74,7 @@ describe('Test for delete Broadcast:', () => {
 
   // Step 2 - delete broadcast test if broadcast doesn't exist for given id:
   it('delete broadcast from DB with wrong id at /broadcast/:id', (done) => {
-    const id = '01';
+    id = '01';
     chai
       .request(server)
       .delete(`/broadcast/${id}`)

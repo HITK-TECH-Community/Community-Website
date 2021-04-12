4aa661193d7ca980db5680e564f71bc715e95649
Aman Jha <aman.jha21.aj@gmail.com>
2021-04-12 17:40:04 +0545

    getBroadcasts route added

--- backend/app/routes/broadcast/index.js
+++ backend/app/routes/broadcast/index.js
@@ -5,7 +5,9 @@ const { authMiddleware } = require('../../../helpers/middlewares/auth');
 const { postBroadcastValidationSchema } = require('./@validationSchema');
 const postBroadcast = require('./postBroadcast');
 const deleteBroadcast = require('./deleteBroadcast');
+const getBroadcasts = require('./getBroadcasts');
 
+router.get('/', getBroadcasts);
 router.post('/', validationMiddleware(postBroadcastValidationSchema), authMiddleware, postBroadcast);
 router.delete('/:id', authMiddleware, deleteBroadcast);
 

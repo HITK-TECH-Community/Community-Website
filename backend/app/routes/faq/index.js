3cf539c22dd929fb3c75ee447341d8522a95218b
iamkriti <69788174+Kriti-bit@users.noreply.github.com>
2021-04-12 23:54:37 +0530

    changed try/catch to await-to-js , DB error , req.body

--- backend/app/routes/faq/index.js
+++ backend/app/routes/faq/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router({ mergeParams: true });
 const faq = require('./post');
-const faqSchema = require('./@validationSchema');
+const FAQValidationSchema = require('./@validationSchema');
 const validation = require('../../../helpers/middlewares/validation');
 
 router.use('/faq', validation(FAQValidationSchema), faq);
--- backend/app/routes/faq/post.js
+++ backend/app/routes/faq/post.js
@@ -1,37 +1,21 @@
-const express = require ("express");
 const FAQ = require('../../models/faq');
 const { ErrorHandler } = require('../../../helpers/error');
 const constants = require('../../../constants');
+const to = require('await-to-js').default;
 
 module.exports = async (req, res, next) => {
-    try {        
-        const {
-            question,
-            answer,
-            isActive,
-            tags,
-            }=req.body;
 
-        const faq = {};
-
-        faq.question = question;
-        faq.answer = answer;
-        if(isActive) faq.isActive = isActive;
-        if(tags) faq.tags = tags; 
-
-        const Faq = new FAQ(faq)
-
-        await Faq.save();
-       
-        res.json(Faq);
-        res.status(200);
-        return next();
-    } catch (err) {
-        const error = new ErrorHandler(constants.ERRORS.UNEXPECTED, {
-          statusCode: '500',
-          message: 'The server encountered an unexpected condition which prevented it from fulfilling the request.',
-          errStack: err,
-        });
-        return next(error);
-      }
-}
+  const [err] = await to(FAQ.create({ ...req.body }));
+  if (err) {
+    const error = new ErrorHandler(constants.ERRORS.DATABASE, {
+      statusCode: 500,
+      message: 'Database Error',
+      errStack: err,
+    });
+    return next(error);
+  }
+  res.status(200).send({
+    message: 'FAQ has been added',
+  });
+  return next();
+ }

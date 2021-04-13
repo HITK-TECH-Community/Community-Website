df1530ded143b282caab5d004ce48c45143ad5be
Aman Jha <aman.jha21.aj@gmail.com>
2021-04-14 00:37:23 +0545

    get broadcasts test updated

--- backend/tests/routes/broadcast/getBroadcasts.test.js
+++ backend/tests/routes/broadcast/getBroadcasts.test.js
@@ -10,15 +10,14 @@ describe('Test for get Broadcasts:', () => {
   it('get broadcasts from DB at /broadcast', (done) => {
     const query = {
       page: 2,
-      limit: 2,
+      tags: 'tag1,tag2',
     };
 
     chai
       .request(server)
-      .get(`/broadcast?page=${query.page}&limit=${query.limit}`)
+      .get(`/broadcast?page=${query.page}&tags=${query.tags}`)
       .then((res) => {
         expect(res.status).to.equal(200);
-        expect(res.body.results.length).to.equal(query.limit);
         done();
       })
       .catch(done);
@@ -27,15 +26,14 @@ describe('Test for get Broadcasts:', () => {
   it('get broadcasts from DB at /broadcast with wrong info', (done) => {
     const query = {
       page: 0,
-      limit: null,
     };
 
     chai
       .request(server)
-      .get(`/broadcast?page=${query.page}&limit=${query.limit}`)
+      .get(`/broadcast?page=${query.page}`)
       .then((res) => {
         expect(res.status).to.equal(400);
-        expect(res.body.message).to.equal('Input Error: Fetching Failed');
+        expect(res.body.message).to.equal('"page" must be greater than or equal to 1');
         done();
       })
       .catch(done);

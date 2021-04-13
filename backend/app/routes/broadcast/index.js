c3f7916a8236538e58191bda58be8ae2e0d45d28
Aman Jha <aman.jha21.aj@gmail.com>
2021-04-14 00:22:27 +0545

    validation schema gor getBroadcasts

--- backend/app/routes/broadcast/index.js
+++ backend/app/routes/broadcast/index.js
@@ -2,12 +2,12 @@ const router = require('express').Router({ mergeParams: true });
 const validationMiddleware = require('../../../helpers/middlewares/validation');
 const { authMiddleware } = require('../../../helpers/middlewares/auth');
 
-const { postBroadcastValidationSchema } = require('./@validationSchema');
+const { postBroadcastValidationSchema, getBroadcastsValidationSchema } = require('./@validationSchema');
 const postBroadcast = require('./postBroadcast');
 const deleteBroadcast = require('./deleteBroadcast');
 const getBroadcasts = require('./getBroadcasts');
 
-router.get('/', getBroadcasts);
+router.get('/', validationMiddleware(getBroadcastsValidationSchema, 'query'), getBroadcasts);
 router.post('/', validationMiddleware(postBroadcastValidationSchema), authMiddleware, postBroadcast);
 router.delete('/:id', authMiddleware, deleteBroadcast);
 

ff9ca13bb0dcc73b4741dad33a97319b5ad3b496
Aman Jha <aman.jha21.aj@gmail.com>
2021-04-24 12:06:07 +0545

    next previous removed and filter by year & month added

--- backend/app/routes/broadcast/getBroadcasts.js
+++ backend/app/routes/broadcast/getBroadcasts.js
@@ -7,32 +7,47 @@ const limit = constants.PAGINATION_LIMIT.GET_BROADCASTS;
 
 const getBroadcastsAggregate = (startIndex, match) => {
   const pipeline = [
-    { $match: match },
     {
       $project: {
         _id: 1,
         title: 1,
-        // imageUrl: 1,
-        // link: 1,
-        // content: 1,
-        // tags: 1,
-        // expiresOn: 1,
+        imageUrl: 1,
+        link: 1,
+        content: 1,
+        tags: 1,
+        expiresOn: 1,
         createdAt: 1,
-        // updatedAt: 1,
+        updatedAt: 1,
+        year: { $year: '$createdAt' },
+        month: { $month: '$createdAt' },
       },
     },
+    { $match: match },
+    { $unset: ['year', 'month'] },
     { $sort: { createdAt: -1 } },
-    { $skip: startIndex && startIndex - 1 },
-    { $limit: startIndex ? limit + 2 : limit + 1 },
+    { $skip: startIndex },
+    { $limit: limit },
   ];
   return pipeline;
 };
 module.exports = async (req, res, next) => {
   const page = parseInt(req.query.page, 10) || 1;
-  // match for tag filtering
-  const match = req.query.tags ? { tags: { $in: req.query.tags.split(',') } } : {};
+  const tags = req.query.tags?.split(',');
+  const year = parseInt(req.query.year, 10);
+  const month = parseInt(req.query.month, 10);
+  // match for filtering
+  const match = {};
+  if (tags) {
+    match.tags = { $in: tags };
+  }
+  if (year) {
+    match.year = year;
+  }
+  if (month) {
+    match.month = month;
+  }
   const startIndex = (page - 1) * limit;
-  const results = {};
+  let results = {};
   const [err, broadcasts] = await to(Broadcast.aggregate(getBroadcastsAggregate(startIndex, match)).exec());
   if (err) {
     const error = new ErrorHandler(constants.ERRORS.DATABASE, {
@@ -42,16 +57,7 @@ module.exports = async (req, res, next) => {
     });
     return next(error);
   }
-  // check if previous page exists
-  if (startIndex > 0) {
-    [results.previous] = broadcasts;
-  }
-  // check if next page exists
-  if (startIndex === 0 || broadcasts.length === limit + 2) {
-    results.next = broadcasts.slice(-1);
-  }
-  // current broadcasts
-  results.results = startIndex ? broadcasts.slice(1, limit + 1) : broadcasts.slice(0, limit);
+  results = broadcasts.slice(0, limit);
   res.status(200).json(results);
   return next();
 };

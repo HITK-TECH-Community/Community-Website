a48fedd54cacbba8dbacdb383ed018ff3137f0e1
Arin Modi <arinmodi2306@gmail.com>
2022-01-23 16:29:18 +0530

    🍀 [Backend] POST API For Questions under Q&A (#703)

---
+++ backend/app/routes/question/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router({ mergeParams: true });
+const postQuestion = require('./post');
+const QuestionValidationSchema = require('./@validationSchema');
+const validation = require('../../../helpers/middlewares/validation');
+
+router.post('/', validation(QuestionValidationSchema), postQuestion);
+
+module.exports = router;
---
+++ backend/app/routes/question/post.js
@@ -0,0 +1,24 @@
+const to = require('await-to-js').default;
+const QUESTION = require('../../models/question');
+const { ErrorHandler } = require('../../../helpers/error');
+const constants = require('../../../constants');
+
+module.exports = async (req,res,next) => {
+    const [ err ] = await to(QUESTION.create({ ...req.body }));
+
+    if(err){
+        const error = new ErrorHandler(constants.ERRORS.DATABASE,{
+            statusCode: 500,
+            message: 'Database Error',
+            errStack: err,
+        });
+
+        return next(error);
+    }
+
+    res.status(200).send({
+        message: 'Question has been added',
+    });
+
+    return next();
+}

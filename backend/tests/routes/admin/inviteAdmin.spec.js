d0fc98fb5cef3e5fc34556702b6672e44b83de64
Arin Modi <arinmodi2306@gmail.com>
2022-01-24 15:44:02 +0530

    [Backend] Fix the failing UTs #704

--- backend/tests/routes/admin/inviteAdmin.spec.js
+++ backend/tests/routes/admin/inviteAdmin.spec.js
@@ -1,59 +1,59 @@
-// const chai = require('chai');
-// const chaiHttp = require('chai-http');
-// const server = require('../../../index');
+/*const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../../../index');
 
-// // Assertion Style
-// chai.should();
+// Assertion Style
+chai.should();
 
-// chai.use(chaiHttp);
+chai.use(chaiHttp);
 
-// describe('Invite Admin', () => {
-//   let token = ''
+describe('Invite Admin', () => {
+  let token = ''
 
-//   // Step 1 : login as admin
+  // Step 1 : login as admin
 
-//   it('login as admin at auth/token', (done) => {
-//     // First we login using the admin credentials to get the auth token
-//     const loginBody = {
-//       email: 'kajolkumarisingh222@gmail.com',
-//       password: 'password',
-//     };
+  it('login as admin at auth/token', (done) => {
+    // First we login using the admin credentials to get the auth token
+    const loginBody = {
+      email: 'kajolkumarisingh222@gmail.com',
+      password: 'password',
+    };
 
-//     chai
-//       .request(server)
-//       .post('/auth/login')
-//       .send(loginBody)
-//       .end((err, response) => {
-//         response.should.have.status(200);
-//         response.body.should.have.property('token');
-//         token = response.body.token;
-//         done();
-//       });
-//   });
+    chai
+      .request(server)
+      .post('/auth/login')
+      .send(loginBody)
+      .end((err, response) => {
+        response.should.have.status(200);
+        response.body.should.have.property('token');
+        token = response.body.token;
+        done();
+      });
+  });
 
-//   // Step 2 : invite admin
-//   it('invite admin',(done) => {
-//     const inviteBody = {
-//       email: 'priyanshus.edu@gmail.com',
-//       data: {
-//         name: 'Priyanshu Sharma',
-//         link: 'https://www.google.com',
-//       },
-//     };
+  // Step 2 : invite admin
+  it('invite admin',(done) => {
+    const inviteBody = {
+      email: 'priyanshus.edu@gmail.com',
+      data: {
+        name: 'Priyanshu Sharma',
+        link: 'https://www.google.com',
+      },
+    };
 
-//     chai
-//       .request(server)
-//       .post('/admin/inviteAdmin')
-//       .set('Authorization', `Bearer ${token}`)
-//       .send(inviteBody)
-//       .end((err2, response2) => {
-//         console.log(response2.body);
-//         response2.should.have.status(200);
-//         response2.body.should.have.property('message');
-//         response2.body.message.should.equal('Email successfully sent');
-//         done();
-//       });
-//   })
+    chai
+      .request(server)
+      .post('/admin/inviteAdmin')
+      .set('Authorization', `Bearer ${token}`)
+      .send(inviteBody)
+      .end((err2, response2) => {
+        console.log(response2.body);
+        response2.should.have.status(200);
+        response2.body.should.have.property('message');
+        response2.body.message.should.equal('Email successfully sent');
+        done();
+      });
+  })
 
   
-// });
+});*/

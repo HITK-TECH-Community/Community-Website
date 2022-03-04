60f70e43c251620473b6548d75042141935f7031
Aman saxena <74807466+Aman16-ai@users.noreply.github.com>
2022-03-04 22:16:34 +0530

    🌼 GET Question/Questions(#732)

--- backend/app/routes/Q&A/question/index.js
+++ backend/app/routes/Q&A/question/index.js
@@ -1,8 +1,15 @@
 const router = require('express').Router({ mergeParams: true });
 const postQuestion = require('./post');
 const QuestionValidationSchema = require('./@validationSchema');
+const getAllQuestion = require('./getAllQuestion');
+const getQuestionById = require('./getQuestionById');
 const validation = require('../../../../helpers/middlewares/validation');
 
 router.post('/', validation(QuestionValidationSchema), postQuestion);
 
+//this route will give all questions from db
+router.get('/getallquestions', getAllQuestion);
+
+//this route will give question by given id
+router.get('/getQuestionById', getQuestionById);
 module.exports = router;

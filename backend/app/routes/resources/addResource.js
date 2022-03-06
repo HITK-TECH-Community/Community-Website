6765515d256e9dc77a69222aa6d5da1856fb2c3b
Adnan Muzavor <www.adnan9168389872@gmail.com>
2022-03-06 17:25:56 +0530

    Removed unnecessary comments

--- backend/app/routes/resources/addResource.js
+++ backend/app/routes/resources/addResource.js
@@ -1,17 +1,11 @@
 const to = require('await-to-js').default;
-//Getting resource modal
 const Resource = require('../../models/resource');
-//Getting resource handler
 const { ErrorHandler } = require('../../../helpers/error');
 const constants = require('../../../constants');
-//Getting admin model and sendEmail utility
 const Admin = require('../../models/Admin');
 const sendEmail = require('../../../utility/sendEmail');
-//Getting mail template
 const { ResourceAddedInformingMailTemplate } = require('../../../utility/emailTemplates');
-//ResourceAddedInformingMailTemplate
 module.exports = async (req, res, next) => {
-  //Adding resource
   const [err] = await to(Resource.create({ ...req.body }));
   if (err) {
     const error = new ErrorHandler(constants.ERRORS.DATABASE, {
@@ -21,7 +15,6 @@ module.exports = async (req, res, next) => {
     });
     return next(error);
   }
-  //Finding Admin
   const [err1, response] = await to(Admin.find().select('email username'));
   if (err1) {
     const error = new ErrorHandler(constants.ERRORS.DATABASE, {
@@ -31,7 +24,6 @@ module.exports = async (req, res, next) => {
     });
     return next(error);
   }
-  //Sending email to admin
   try {
     response.map(async (adminUser) => {
       await sendEmail(
@@ -48,10 +40,8 @@ module.exports = async (req, res, next) => {
     });
     return next(error);
   }
-  //Finally giving user message
   res.status(200).send({
     message: 'Resource has been added',
   });
   return next();
-  
 };
--- backend/app/routes/resources/index.js
+++ backend/app/routes/resources/index.js
@@ -1,15 +1,6 @@
-//Creating a router
 const router = require('express').Router({ mergeParams: true });
-
-//Getting addResource function which deals with posting data
 const addaResource = require('./addResource');
-
-//Importing middlewatr for validation
 const validation = require('../../../helpers/middlewares/validation');
 const ResourcesValidationSchema = require('./@validationSchema');
-
-//Route by default for post
 router.post('/', validation(ResourcesValidationSchema), addaResource);
-
-//Exporting router to use it in "index.js"
 module.exports = router;

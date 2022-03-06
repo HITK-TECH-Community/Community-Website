4546b30015df901d5c76de4b72c53a86f85dc3a3
Adnan Muzavor <www.adnan9168389872@gmail.com>
2022-03-06 22:51:32 +0530

    Added API for getting resources

--- backend/app/routes/resources/index.js
+++ backend/app/routes/resources/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router({ mergeParams: true });
 const addaResource = require('./addResource');
+const getResource = require('./getResource');
 const validation = require('../../../helpers/middlewares/validation');
 const ResourcesValidationSchema = require('./@validationSchema');
+const { authMiddleware } = require('../../../helpers/middlewares/auth');
 router.post('/', validation(ResourcesValidationSchema), addaResource);
+router.get('/getresources', authMiddleware, getResource);
 module.exports = router;

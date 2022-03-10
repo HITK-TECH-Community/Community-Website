21a33531fc25eaa14d97f9f2403ebc63f38c0591
kajol <kajolkumarisingh222@gmail.com>
2022-03-10 16:38:01 +0530

    Removed `age` from schema

--- backend/app/routes/contactUs/@validationSchema/index.js
+++ backend/app/routes/contactUs/@validationSchema/index.js
@@ -4,8 +4,7 @@ const contactValidationSchema = Joi.object().keys({
   name: Joi.string().trim().required().min(3),
   email: Joi.string().trim().email().required(),
   subject: Joi.string().trim().required().min(5),
-  message: Joi.string().trim().required().min(8),
-  age: Joi.number().required(),
+  message: Joi.string().trim().required().min(8)
 });
 
 module.exports = contactValidationSchema;

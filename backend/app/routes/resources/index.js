ce2b1d95ea0e0e3d9438e2dc5a8615fad7c3db44
kajol <kajolkumarisingh222@gmail.com>
2022-03-13 10:53:20 +0530

    Removed unwanted changes

--- backend/app/routes/resources/index.js
+++ backend/app/routes/resources/index.js
@@ -1,20 +1,14 @@
 
 const router = require('express').Router({ mergeParams: true });
 const addaResource = require('./addResource');
-<<<<<<< HEAD
 const deleteResource = require('./deleteResource');
-=======
 const getResource = require('./getResource');
->>>>>>> 35a4d66ba933238c7596691c6f15b171ae7f100a
 const validation = require('../../../helpers/middlewares/validation');
 const ResourcesValidationSchema = require('./@validationSchema');
 const { authMiddleware } = require('../../../helpers/middlewares/auth');
 router.post('/', validation(ResourcesValidationSchema), addaResource);
-<<<<<<< HEAD
 
 //route for deleting a resource
 router.delete('/deleteResource', deleteResource);
-=======
 router.get('/getresources', authMiddleware, getResource);
->>>>>>> 35a4d66ba933238c7596691c6f15b171ae7f100a
 module.exports = router;

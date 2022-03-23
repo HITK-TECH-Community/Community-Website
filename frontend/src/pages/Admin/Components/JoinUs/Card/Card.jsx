ab444a52fe26c765f9c22ee0278a9559d93c6c53
Aman saxena <asaxena7531@gmail.com>
2022-03-23 13:11:00 +0530

    Improving card ui and formatting the code

--- frontend/src/pages/Admin/Components/JoinUs/Card/Card.jsx
+++ frontend/src/pages/Admin/Components/JoinUs/Card/Card.jsx
@@ -1,5 +1,5 @@
 import { IconButton } from "@material-ui/core";
-import { Edit } from "@material-ui/icons";
+import { Edit,Delete } from "@material-ui/icons";
 import style from "./card.module.scss";
 
 export function Card(props) {
@@ -14,11 +14,16 @@ export function Card(props) {
                 <h3 className={style["card-detail"]}>Year : {props.content.year}</h3>
                 <h3 className={style['card-detail']}>{props.content.contact}</h3>
                 <p>{props.content.description}</p>
-                <p>hazem@test.com</p>
+                <p>{props.content.email}</p>
                 <a className={style['card-link']} href={props.content.linkedin}>Linkdin</a>
-                <IconButton className={style['icon-button']}>
-                    <Edit/>
+                <div className={style['button-container']}>
+                <IconButton className={style['icon-button']} >
+                    <Edit className={style['icon']}/>
                 </IconButton>
+                <IconButton className={style['icon-button']} >
+                    <Delete className={style['icon']}/>
+                </IconButton>
+                </div>
             </div>
 
         </div>

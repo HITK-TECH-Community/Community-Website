d5ca1b7f56d2f6cfe05717e5adafa03d7b30f6bc
Vineeth <72906276+vinrusty@users.noreply.github.com>
2022-04-06 10:40:57 +0530

    🍁 [Backend] Added Error Handlers In Add Team Api (#781)
    * Removing all eslint errors in backend
    
    * Integrated Delete feature in Resources tab
    
    * Adding error handlers into addTeam
    
    Co-authored-by: vinrusty <vineethhr2016@gmail.com>

--- backend/app/routes/teamMember/addTeam.js
+++ backend/app/routes/teamMember/addTeam.js
@@ -1,7 +1,9 @@
+const to = require('await-to-js').default;
+const { ErrorHandler } = require('../../../helpers/error');
+const constants = require('../../../constants');
 const TeamMemberModel = require('../../models/TeamMember');
 
-module.exports = async (req, res) => {
-  console.log(req.file);
+module.exports = async (req, res, next) => {
   const payload = res.locals.decode;
   if (payload.isSuperAdmin === false) {
     res.status(401).json({ error: 'You are not an admin' });
@@ -17,6 +19,17 @@ module.exports = async (req, res) => {
     twitter_url: twitterUrl,
     teams,
   });
-  const result = await teammember.save();
-  return res.json({ result });
+
+  const [err, result] = await to(teammember.save());
+  if (err) {
+    const error = new ErrorHandler(constants.ERRORS.DATABASE, {
+      statusCode: 500,
+      message: 'Database Error',
+      errStack: err,
+    });
+    return next(error);
+  }
+
+  res.status(200).json({ result });
+  return next();
 };

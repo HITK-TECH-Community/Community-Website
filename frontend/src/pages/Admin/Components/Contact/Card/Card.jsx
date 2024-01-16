72b405201d16263eb5b2fe7ce060291f1ddbd508
Aditya Nandan <61308761+iMADi-ARCH@users.noreply.github.com>
2024-01-16 09:54:28 +0530

    feat: show contactus data on admin dashboard (#829)
    * feat: show contactus data on admin dashboard
    
    * style: change colors of buttons on contactus and joinus
    
    * style: change colors of buttons on contactus and joinus

---
+++ frontend/src/pages/Admin/Components/Contact/Card/Card.jsx
@@ -0,0 +1,20 @@
+import style from "./card.module.scss";
+
+export function Card({ content: { email, message, name, subject } }) {
+  return (
+    <div className={style["card-item"]}>
+      <div className={style["card-info"]}>
+        <h1>{name}</h1>
+        <h3 className={style["card-detail"]}>Email : {email}</h3>
+        <h3 className={style["card-detail"]}>Subject : {subject}</h3>
+        <h2 className={style["card-detail"]}>{message}</h2>
+        <div className={style["button-group"]}>
+          <a href={`mailto:${email}`}>
+            <button className={style["button-edit"]}>Reply</button>
+          </a>
+          <button className={style["button-delete"]}>Delete</button>
+        </div>
+      </div>
+    </div>
+  );
+}

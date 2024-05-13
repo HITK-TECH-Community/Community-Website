7d68ea476bb3067bb3ed428e778bf0c2f76c6e91
Hemu21 <rajva5727@gmail.com>
2024-05-13 20:32:00 +0530

    required changes

--- frontend/src/pages/Admin/Components/MangeTeams/ManageTeams.jsx
+++ frontend/src/pages/Admin/Components/MangeTeams/ManageTeams.jsx
@@ -16,7 +16,7 @@ export function ManageTeams() {
       });
       const res = await response.json()
       setCards(res)
-      console.log(res)
+      console.log(res[0])
     } catch (error) {
       console.log(error)
       alert("Failed to fetch!")
@@ -36,20 +36,20 @@ export function ManageTeams() {
             key={d.full_name}
           >
             <div style={{display:"flex", justifyContent:"center",alignItems:"center"}}>
-              <img src={d.image} style={{margin:"auto", borderRadius:"50%", width:"200px", height:"200px"}} alt="" />
+              <img src={d.image || "./images/defaultUser.png"} style={{margin:"auto", borderRadius:"50%", width:"200px", height:"200px"}} alt="" />
             </div>
             <h4> {d.full_name} </h4>
             <div className={style["content"]}>
               <h3 style={{fontWeight:"bolder"}}>About</h3> {d.description}
             </div>
             <div className={style["content"]}>
-            <h3 style={{fontWeight:"bold"}}>LinkedIn</h3>{d.linkedin_url}
+            <h3 style={{fontWeight:"bold"}}>LinkedIn</h3><a href={d.linkedin_url} target="_blank" rel="noopener noreferrer" >{d.linkedin_url&&`${d.linkedin_url?.slice(0,10)}...${d.linkedin_url?.slice(d.linkedin_url?.length - 10)}`}</a>
             </div>
             <div className={style["content"]}>
-            <h3 style={{fontWeight:"bold"}}>GitHub</h3>{d.github_url}
+            <h3 style={{fontWeight:"bold"}}>GitHub</h3><a href={d.github_url} target="_blank" rel="noopener noreferrer" >{d.github_url&&`${d.github_url?.slice(0,10)}...${d.github_url?.slice(d.github_url?.length - 10)}`}</a>
             </div>
             <div className={style["content"]}>
-            <h3 style={{fontWeight:"bold"}}>Twitter</h3>{d.twitter_url}
+            <h3 style={{fontWeight:"bold"}}>Twitter</h3><a href={d.twitter_url} target="_blank" rel="noopener noreferrer" >{d.twitter_url&&`${d.twitter_url?.slice(0,10)}...${d.twitter_url?.slice(d.twitter_url?.length - 10)}`}</a>
             </div>
             <div className={style["content"]}>
             <h3 style={{fontWeight:"bold"}}>Team</h3>{d.teams?.join(", ")}

dde031ba93ab14696d94d010757cf1c00c2f6066
Hemu21 <rajva5727@gmail.com>
2024-05-13 14:11:19 +0530

    Team members are populated

--- frontend/src/pages/Admin/Components/MangeTeams/ManageTeams.jsx
+++ frontend/src/pages/Admin/Components/MangeTeams/ManageTeams.jsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from "./manage-teams.module.scss";
-import cards from "../../../../test_data/teams"
+import { END_POINT } from "../../../../config/api";
 
 export function ManageTeams() {
+  const [cards,setCards] = useState([])
+  const getdata = async ()=>{
+    try {
+      const url = `${END_POINT}/teamMember/getTeamMembers/`
+      const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem('token')}`
+        },
+      });
+      const res = await response.json()
+      setCards(res)
+      console.log(res)
+    } catch (error) {
+      console.log(error)
+      alert("Failed to fetch!")
+    }
+    
+  }
+  useEffect(()=>{
+    getdata()
+  },[])
   return (
     <div>
       <h1 className={style["head"]}>Teams</h1>
@@ -10,14 +33,26 @@ export function ManageTeams() {
         {cards.map((d) => (
           <div
             className={style["crd"]}
-            key={d.name}
+            key={d.full_name}
           >
-            <div className={style["head1"]}>
-              {d.icon}
-              <h4> {d.name} </h4>
+            <div style={{display:"flex", justifyContent:"center",alignItems:"center"}}>
+              <img src={d.image} style={{margin:"auto", borderRadius:"50%", width:"200px", height:"200px"}} alt="" />
+            </div>
+            <h4> {d.full_name} </h4>
+            <div className={style["content"]}>
+              <h3 style={{fontWeight:"bolder"}}>About</h3> {d.description}
             </div>
             <div className={style["content"]}>
-              {d.description}
+            <h3 style={{fontWeight:"bold"}}>LinkedIn</h3>{d.linkedin_url}
+            </div>
+            <div className={style["content"]}>
+            <h3 style={{fontWeight:"bold"}}>GitHub</h3>{d.github_url}
+            </div>
+            <div className={style["content"]}>
+            <h3 style={{fontWeight:"bold"}}>Twitter</h3>{d.twitter_url}
+            </div>
+            <div className={style["content"]}>
+            <h3 style={{fontWeight:"bold"}}>Team</h3>{d.teams?.join(", ")}
             </div>
           </div>
         ))}

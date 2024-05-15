be6ac5c544c65ea850a163fb142f55212584f90f
swayamName1 <swayamrana808@gmail.com>
2024-05-15 13:08:36 +0530

    local storage clean up

--- frontend/src/store/actions/auth.js
+++ frontend/src/store/actions/auth.js
@@ -2,5 +2,8 @@ export const logout = (dispatch) => {
   localStorage.removeItem("token");
   localStorage.removeItem("isSuperAdmin");
   localStorage.setItem("log", true);
+  localStorage.removeItem("firstName");
+  localStorage.removeItem("email");
+  localStorage.removeItem("phone");
   window.location = "/admin";
 };

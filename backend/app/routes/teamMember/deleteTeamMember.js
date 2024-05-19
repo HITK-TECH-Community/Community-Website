a53acb609dd18f973495cfe9eb27ab2affb6a834
Hemanth kumar <rajva5727@gmail.com>
2024-05-19 11:22:06 +0530

    Update deleteTeamMember.js

--- backend/app/routes/teamMember/deleteTeamMember.js
+++ backend/app/routes/teamMember/deleteTeamMember.js
@@ -6,25 +6,18 @@ module.exports = async (req, res, next) => {
   try {
     const payload = res.locals.decode;
     const memberId = req.body.memberId;
-
     if (payload.isSuperAdmin === false) {
       return res.status(401).json({ error: 'You are not an admin' });
     }
-
-    const member = await teamMemberModel.findById(memberId);
-    if (!member) {
-      return res.status(401).json({ error: 'Invalid id' });
-    }
-
     const result = await teamMemberModel.findByIdAndDelete(memberId);
     if (!result) {
       return res.status(401).json({ error: 'Invalid id' });
     }
-
-    const fileName = path.basename(member.image);
-    const imagePath = path.join(__dirname, '../../../uploads/teamMembersProfile/', fileName);
-    fs.unlink(imagePath, (err) => console.log(err));
-
+    if (result?.image) {
+      const fileName = path.basename(result.image);
+      const imagePath = path.join(__dirname, '../../../uploads/teamMembersProfile/', fileName);
+      fs.unlink(imagePath, (err) => console.log(err));
+    }
     return res.json({ message: 'Deleted successfully' });
   } catch (error) {
     console.error(error);

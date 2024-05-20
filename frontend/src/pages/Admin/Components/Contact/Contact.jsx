60fde3a638511349a43fd394558a43c71fab33a1
Hemanth kumar <rajva5727@gmail.com>
2024-05-20 21:31:28 +0530

    🍁 [Frontend] Integrate Delete Contact Us API Fixed (#934)
    * delete contact us
    
    * Update Contact.jsx

--- frontend/src/pages/Admin/Components/Contact/Contact.jsx
+++ frontend/src/pages/Admin/Components/Contact/Contact.jsx
@@ -5,12 +5,20 @@ import Grid from "@material-ui/core/Grid";
 import { Card } from "./Card/index.js";
 import style from "./contactus.module.scss";
 import Loader from "../../../../components/util/Loader";
+import { SimpleToast } from "../../../../components/util/Toast/Toast.jsx";
 
 export function Contact() {
   const [contactUsData, setContactUsData] = useState([]);
   const [isLoaded, setIsLoaded] = useState(false);
+  const [toast, setToast] = useState({
+    toastStatus: false,
+    toastType: "",
+    toastMessage: "",
+  });
   const fetchJoinUs = async () => {
-    const response = await fetch(`${END_POINT}/getContactUs`, {
+    setIsLoaded(true);
+    try{
+    const response = await fetch(`${END_POINT}/contactus/getcontactus`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -19,7 +27,57 @@ export function Contact() {
     });
     const data = await response.json();
     setContactUsData(data.ContactUs);
+    setToast({
+        ...toast,
+        toastMessage: "Successfully get data!",
+        toastStatus: true,
+        toastType: "success",
+      });
+    }catch(error){
+      setToast({
+        ...toast,
+        toastMessage: "Unable to get data!",
+        toastStatus: true,
+        toastType: "error",
+      });
+    }
     setIsLoaded(false);
+  };
+  const handleCloseToast = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setToast({ ...toast, toastStatus: false });
+  };
+  const handleDelete = async (id) => {
+    try {
+      const url = `${END_POINT}/contactus/deleteContactUs`;
+      const response = await fetch(url, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+        body: JSON.stringify({ contactUsId: id }),
+      });
+
+      const data = await response.json();
+      setToast({
+        ...toast,
+        toastMessage: "Successfully deleted!",
+        toastStatus: true,
+        toastType: "success",
+      });
+      fetchJoinUs()
+    } catch (error) {
+      console.log(error);
+      setToast({
+        ...toast,
+        toastMessage: "Unable to delete!",
+        toastStatus: true,
+        toastType: "error",
+      });
+    }
   };
   useEffect(() => {
     setIsLoaded(true);
@@ -28,17 +86,25 @@ export function Contact() {
   return (
     <div>
       <h1 style={{ textAlign: "center" }}> Contact Us </h1>
-      <div className={style["data-loader"]}>{isLoaded ? <Loader /> : null}</div>
+      {isLoaded ? <div className={style["data-loader"]}><Loader /></div>:
       <div className={style["card-container"]}>
         <Grid container spacing={2}>
           <Grid item>
             {contactUsData &&
               contactUsData.map((data) => {
-                return <Card key={data._id} content={data} />;
+                return <Card key={data._id} id={data._id} handleDelete={handleDelete} content={data} />;
               })}
           </Grid>
         </Grid>
-      </div>
+      </div>}
+      {toast.toastStatus && (
+        <SimpleToast
+          open={toast.toastStatus}
+          message={toast.toastMessage}
+          handleCloseToast={handleCloseToast}
+          severity={toast.toastType}
+        />
+      )}
     </div>
   );
 }

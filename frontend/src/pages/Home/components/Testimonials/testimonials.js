fcf4c4226bccd49781d2305af0f3be251eb868d2
nagalakshmi08 <lakshmireddydwarampudi436@gmail.com>
2024-06-07 20:38:34 +0530

    Testimonials

---
+++ frontend/src/pages/Home/components/Testimonials/testimonials.js
@@ -0,0 +1,49 @@
+const testimonials = [
+  {
+    name: "John Doe",
+    position: "Care Seeker",
+    company: "Nacto Care",
+    image:
+      "https://dpemoji.com/wp-content/uploads/2023/01/Whatsapp-dp-for-boys-57.png",
+    text: "Nacto Care has been a lifesaver for my family. The nurse we hired through their platform provided excellent care for my elderly parents. The entire process was smooth and professional.",
+    rating: 5,
+  },
+  {
+    name: "Jane Smith",
+    position: "Care Seeker",
+    company: "Nacto Care",
+    image:
+      "https://t3.ftcdn.net/jpg/06/36/69/86/360_F_636698674_DroChEj5eWmZiaZOSDMnj8hcDqqw74Fp.jpg",
+    text: "The personalized care plans and flexible scheduling options offered by Nacto Care made it easy for us to get the help we needed. The nurse was knowledgeable and compassionate.",
+    rating: 5,
+  },
+  {
+    name: "Michael Johnson",
+    position: "Customer",
+    company: "Nacto Care",
+    image:
+      "https://dpemoji.com/wp-content/uploads/2023/01/Whatsapp-dp-for-boys-57.png",
+    text: "I was impressed by the quality of service provided by Nacto Care. The platform is user-friendly, and the nurses are highly qualified. I would highly recommend them to anyone in need of home care services.",
+    rating: 5,
+  },
+  {
+    name: "Emily Brown",
+    position: "Care Seeker",
+    company: "Nacto Care",
+    image:
+      "https://t3.ftcdn.net/jpg/06/36/69/86/360_F_636698674_DroChEj5eWmZiaZOSDMnj8hcDqqw74Fp.jpg",
+    text: "Nacto Care connected us with a fantastic nurse who took great care of my grandfather. The support and attention to detail were outstanding. Thank you for making a difference in our lives.",
+    rating: 4.5,
+  },
+  {
+    name: "Alex Williams",
+    position: "Customer",
+    company: "Nacto Care",
+    image:
+      "https://dpemoji.com/wp-content/uploads/2023/01/Whatsapp-dp-for-boys-57.png",
+    text: "The life insurance scheme offered by Nacto Care gives us peace of mind knowing that our loved ones are protected. The nurse we hired was professional and dedicated to providing the best care possible.",
+    rating: 4,
+  },
+];
+
+export default testimonials;

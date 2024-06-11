56bcd5500b4c1d05e63de877ca2cd65ee7701a09
Shivam Gaur <shivamgaur8527@gmail.com>
2024-06-11 22:23:00 +0530

    Create POST Testimonial API

--- backend/app/routes/testimonial/index.js
+++ backend/app/routes/testimonial/index.js
@@ -1,7 +1,13 @@
 const router = require('express').Router({ mergeParams: true });
+const validationMiddleware = require('../../../helpers/middlewares/validation');
+const { authMiddleware } = require('../../../helpers/middlewares/auth');
 
+const { postTestimonialValidationSchema } = require('./@validationSchema');
+const postTestimonial = require('./postTestimonial');
 const getTestimonials = require('./getTestimonials');
 
+router.post('/', validationMiddleware(postTestimonialValidationSchema), authMiddleware, postTestimonial);
 router.get('/getTestimonials', getTestimonials);
+
 
 module.exports = router;
---
+++ backend/app/routes/testimonial/postTestimonial.js
@@ -0,0 +1,21 @@
+const to = require('await-to-js').default;
+const Testimonial = require('../../models/Testimonial');
+const { ErrorHandler } = require('../../../helpers/error');
+const constants = require('../../../constants');
+
+module.exports = async (req, res, next) => {
+  const [err, { _id }] = await to(Testimonial.create({ ...req.body }));
+  if (err) {
+    const error = new ErrorHandler(constants.ERRORS.DATABASE, {
+      statusCode: 500,
+      message: 'Mongo Error: Insertion Failed',
+      errStack: err,
+    });
+    return next(error);
+  }
+  res.status(200).send({
+    message: 'Testimonial added successfully',
+    id: _id,
+  });
+  return next();
+};

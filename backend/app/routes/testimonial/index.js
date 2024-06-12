3fefdb3f0d1ff8d91c578a29c0700fa64d1a1504
Shivam Gaur <128178418+shivamgaur99@users.noreply.github.com>
2024-06-12 22:37:57 +0530

    🍀[Backend] Create DELETE Testimonial API (#1044)

--- backend/app/routes/testimonial/index.js
+++ backend/app/routes/testimonial/index.js
@@ -5,9 +5,10 @@ const { authMiddleware } = require('../../../helpers/middlewares/auth');
 const { postTestimonialValidationSchema } = require('./@validationSchema');
 const postTestimonial = require('./postTestimonial');
 const getTestimonials = require('./getTestimonials');
+const deleteTestimonial = require('./deleteTestimonial');
 
 router.post('/', validationMiddleware(postTestimonialValidationSchema), authMiddleware, postTestimonial);
 router.get('/getTestimonials', getTestimonials);
-
+router.delete('/:id', authMiddleware, deleteTestimonial);
 
 module.exports = router;

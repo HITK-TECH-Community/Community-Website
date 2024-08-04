33a9c76f9372857701a656bfff1ee1554ac9966d
Balaharisankar Lakshmanaperumal <114602603+BHS-Harish@users.noreply.github.com>
2024-08-04 07:11:26 +0530

    ⚙️ UI to answer the questions in Q&A Section Added (#1104)
    * Upvote and Downvotes fixed
    
    * Feat: Post answer added in Q&A Section

--- frontend/src/pages/Q&A/Q&A.jsx
+++ frontend/src/pages/Q&A/Q&A.jsx
@@ -12,6 +12,7 @@ import {
   downvote,
 } from "../../service/Faq";
 import { showToast, hideToast } from "../../service/toastService";
+import { AnswerModel } from './AnswerModel/index'
 
 function Ques(props) {
   let dark = props.theme;
@@ -36,6 +37,8 @@ function Ques(props) {
   const [checkedState, setCheckedState] = useState(
     new Array(Tags.length).fill(false)
   );
+  const [open, setOpen] = useState(false)
+  const [currentQuestion, setCurrentQuestion] = useState({})
   const [toast, setToast] = useState({
     toastStatus: false,
     toastType: "",
@@ -160,9 +163,10 @@ function Ques(props) {
 
   return (
     <div
-    className="popup-creator"
-    style={{ background: dark ? "#171717" : "white" }}
-  >
+      className="popup-creator"
+      style={{ background: dark ? "#171717" : "white" }}
+    >
+      <AnswerModel theme={dark} open={open} data={currentQuestion} handleClose={setOpen} />
     {
       !loading ?
         <Loader /> :
@@ -204,8 +208,13 @@ function Ques(props) {
                     </button>
                   </div>
                 </div>
+                <button className="answer-btn" onClick={() => {
+                  setCurrentQuestion(item)
+                  setOpen(true)
+                }}>Answers</button>
               </div>
-            ))}
+            )
+            )};
         </div>
       }
       {toast.toastStatus && (
